f1808302740ca0d66241501d7135cdc28a0c4ab4
Deepal Jayasinghe <deepal@apache.org>
2005-10-24 05:29:03 +0000

    I created a small synapse toy , have a look at  that if you have time , it has a ReadMe.txt pls read that nad follow the steps
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/synapse/trunk@327975 13f79535-47bb-0310-9956-ffa450edef68

---
+++ scratch/SynapseToy/core/src/org/apache/synapse/SynapseConstant.java
@@ -0,0 +1,36 @@
+package org.apache.synapse;
+/*
+* Copyright 2004,2005 The Apache Software Foundation.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* @author : Deepal Jayasinghe (deepal@apache.org)
+*
+*/
+
+public class SynapseConstant {
+
+    public static String SYNAPSE_SERVICE ="SynapseServie";
+    public static String SYNAPSE_MODULE ="Synapse";
+    public static String SYNAPSE_OPERATION ="SynapseOp";
+
+    public static String PRIVOUS_RULE_ID ="P_RID";
+    public static String CURRENT_RULE_ID="C_RID";
+    public static String CURRENT_MEDIATOR ="C_ME";
+    public static String RULE_ENGINE ="RuleEngine";
+
+    public static String CURRENT_RULE_OBJECT ="CurrentRuleObject";
+
+
+
+}

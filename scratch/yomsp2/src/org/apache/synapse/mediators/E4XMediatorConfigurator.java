0bbda3f27924590322ecef7f0ef8f14755f22f1d
Paul Fremantle <pzf@apache.org>
2005-11-12 21:52:58 +0000

    YOMSP with updates from IRC chat at least partially working
    git-svn-id: https://svn.apache.org/repos/asf/incubator/synapse/trunk@332845 13f79535-47bb-0310-9956-ffa450edef68

---
+++ scratch/yomsp2/src/org/apache/synapse/mediators/E4XMediatorConfigurator.java
@@ -0,0 +1,17 @@
+package org.apache.synapse.mediators;
+
+import org.apache.axis2.om.OMElement;
+import org.apache.synapse.MediatorConfiguration;
+import org.apache.synapse.MediatorConfigurator;
+
+public class E4XMediatorConfigurator implements MediatorConfigurator {
+
+
+
+	public MediatorConfiguration parse(OMElement el, ClassLoader cl) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
---
+++ scratch/yomsp2/src/org/apache/synapse/ruleEngines/Rule.java
@@ -0,0 +1,50 @@
+package org.apache.synapse.ruleEngines;
+
+import java.util.Iterator;
+
+import org.apache.axis2.om.OMAttribute;
+import org.apache.axis2.om.OMElement;
+import org.apache.synapse.Constants;
+import org.apache.synapse.MediatorConfiguration;
+import org.apache.synapse.MediatorConfigurator;
+import org.apache.synapse.MediatorTypes;
+import org.apache.synapse.SynapseException;
+
+public class Rule {
+	private RuleCondition rc = null;
+	private MediatorConfiguration mc = null;
+	private int mediatorType = 0; 
+	
+	public void init(OMElement ruleContainer, ClassLoader cl) {
+		Iterator it = ruleContainer.getChildrenWithName(Constants.MEDIATOR_Q);
+		if (it.hasNext()) {
+			OMElement med = (OMElement)it.next(); // only use first mediator
+			OMAttribute type = med.getAttribute(Constants.TYPE_ATT_Q);
+			if (type==null) throw new SynapseException("no type declaration on "+med.toString());
+			MediatorConfigurator config = MediatorTypes.getMediatorConfigurator(type.getAttributeValue());
+			mc = config.parse(med, cl);
+			mediatorType = MediatorTypes.getType(type.getAttributeValue());
+		}
+		// deal with QoS apply elements here TODO
+	}
+	
+	//public QoS getQoS();
+	
+	public MediatorConfiguration getMediatorConfiguration() {
+		return mc;
+	}
+	public int getMediatorType() { 
+		return mediatorType;
+	}
+	
+	
+	public RuleCondition getRuleCondition() {
+		return rc;
+	}
+	public void setRuleCondition(RuleCondition rc) {
+		this.rc = rc;
+	}
+	
+	
+
+}

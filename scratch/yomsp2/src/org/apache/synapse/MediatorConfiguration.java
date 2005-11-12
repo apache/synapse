92a6b4f1793e072e4b2be8f76558b6864c27fadc
Paul Fremantle <pzf@apache.org>
2005-11-12 21:49:50 +0000

    tidying up
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/synapse/trunk@332844 13f79535-47bb-0310-9956-ffa450edef68

--- scratch/yomsp2/src/org/apache/synapse/MediatorConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.apache.synapse;
-
-import org.apache.axis2.om.OMElement;
-
-public interface MediatorConfiguration {
-	OMElement getMediatorElement();
-	String getMediatorName();
-	int getMediatorType();
-}

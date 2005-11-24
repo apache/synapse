73eafd438e95c4a262af8aea744da9f7a52ca222
Paul Fremantle <pzf@apache.org>
2005-11-24 19:03:46 +0000

    fix up check in - one of these days I'll figure SVN out :-)
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/synapse/trunk@348783 13f79535-47bb-0310-9956-ffa450edef68

---
+++ scratch/prototype3/src/org/apache/synapse/processors/mediatortypes/ClassMediatorProcessor.java
@@ -0,0 +1,51 @@
+package org.apache.synapse.processors.mediatortypes;
+
+import javax.xml.namespace.QName;
+
+import org.apache.axis2.om.OMAttribute;
+import org.apache.axis2.om.OMElement;
+import org.apache.synapse.Constants;
+import org.apache.synapse.SynapseEnvironment;
+import org.apache.synapse.SynapseException;
+import org.apache.synapse.SynapseMessage;
+import org.apache.synapse.api.Mediator;
+import org.apache.synapse.processors.AbstractProcessor;
+
+public class ClassMediatorProcessor extends AbstractProcessor {
+	private static final QName CLM_Q = new QName(Constants.SYNAPSE_NAMESPACE,
+			"classmediator");
+
+	private Class clazz = null;
+
+	public void compile(SynapseEnvironment se, OMElement el) {
+		super.compile(se, el);
+
+		OMAttribute clsName = el.getAttribute(new QName("class"));
+		if (clsName == null)
+			throw new SynapseException("missing class attribute on element"
+					+ el.toString());
+		try {
+			clazz = se.getClassLoader().loadClass(clsName.getAttributeValue());
+		} catch (ClassNotFoundException e) {
+			throw new SynapseException("class loading error", e);
+		}
+
+	}
+
+	public boolean process(SynapseEnvironment se, SynapseMessage smc) {
+		Mediator m = null;
+
+		try {
+			m = (Mediator) clazz.newInstance();
+		} catch (Exception e) {
+			throw new SynapseException(e);
+		}
+		return m.mediate(smc);
+
+	}
+
+	public QName getTagQName() {
+		return CLM_Q;
+	}
+
+}

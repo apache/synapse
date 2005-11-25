18bf3c951453e808bb5034caf3e42f2be52a09af
Saminda Wishwajith Abeyruwan <saminda@apache.org>
2005-11-25 12:38:09 +0000

    1. Added new test cases 2. Added module.xml for first time dispatching 3. Yet to come maven.xml
    git-svn-id: https://svn.apache.org/repos/asf/incubator/synapse/trunk@348935 13f79535-47bb-0310-9956-ffa450edef68

---
+++ scratch/prototype3/test/org/apache/synapse/spi/XpathProcessorTest.java
@@ -0,0 +1,45 @@
+package org.apache.synapse.spi;
+
+import junit.framework.TestCase;
+import org.apache.axis2.context.MessageContext;
+import org.apache.axis2.addressing.EndpointReference;
+import org.apache.synapse.util.Axis2EvnSetup;
+import org.apache.synapse.SynapseMessage;
+import org.apache.synapse.SynapseEnvironment;
+import org.apache.synapse.axis2.Axis2SOAPMessageContext;
+import org.apache.synapse.axis2.Axis2SynapseEnvironment;
+/*
+* Copyright 2004,2005 The Apache Software Foundation.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+public class XpathProcessorTest extends TestCase {
+     private String synapsexml =
+            "<synapse xmlns=\"http://ws.apache.org/ns/synapse\">\n" +
+                    "<stage name=\"xpath\">\n" +
+                    "    <xpath expr=\"//ns:text\" xmlns:ns=\"urn:text-body\"/>\n" +
+                    "</stage>\n" +
+            "</synapse>";
+    public void testXpathProcessor() throws Exception {
+        MessageContext mc = Axis2EvnSetup.axis2Deployment();
+        SynapseMessage smc = new Axis2SOAPMessageContext(mc);
+        SynapseEnvironment env = new Axis2SynapseEnvironment(
+                Axis2EvnSetup.getSynapseConfigElement(synapsexml),
+                Thread.currentThread().getContextClassLoader());
+        env.injectMessage(smc);
+    }
+}
+
+

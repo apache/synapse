af1703ca6cb35c0edfeb5ee97ebae62e97912db2
Saminda Wishwajith Abeyruwan <saminda@apache.org>
2005-11-29 09:16:11 +0000

    Updated maven.xml and SendBackProcessorTest Updated Axis2FlexibleMEPClient to have the right behavior
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/synapse/trunk@349678 13f79535-47bb-0310-9956-ffa450edef68

--- scratch/prototype3/src/org/apache/synapse/axis2/Axis2FlexibleMEPClient.java
+++ scratch/prototype3/src/org/apache/synapse/axis2/Axis2FlexibleMEPClient.java
@@ -12,120 +12,137 @@ import org.apache.axis2.description.OutInAxisOperation;
 import org.apache.axis2.engine.AxisConfigurationImpl;
 import org.apache.axis2.engine.AxisEngine;
 import org.apache.axis2.om.OMAbstractFactory;
+import org.apache.axis2.om.OMElement;
+import org.apache.axis2.om.OMNamespace;
 import org.apache.axis2.soap.SOAPEnvelope;
 import org.apache.axis2.transport.TransportUtils;
 import org.apache.axis2.util.UUIDGenerator;
 import org.apache.synapse.Constants;
 import org.apache.synapse.SynapseException;
+import org.apache.ws.security.SOAP11Constants;
 
 import javax.xml.namespace.QName;
+import java.util.Iterator;
 
 public class Axis2FlexibleMEPClient {
 
-	// wholesale cut and paste from axis2.clientapi.*
-	public static MessageContext send(MessageContext smc) {
-		try {
+    // wholesale cut and paste from axis2.clientapi.*
+    public static MessageContext send(MessageContext smc) {
+        try {
 
-			ConfigurationContext sysContext = null;
-			ConfigurationContextFactory efac = new ConfigurationContextFactory();
-			sysContext = efac.buildClientConfigurationContext(null);
-			QName assumedServiceName = new QName("AnonymousService");
-			AxisService axisService = new AxisService(assumedServiceName);
-			AxisOperation axisOperationTemplate = new OutInAxisOperation(
-					new QName("TemplateOperation"));
-			PhasesInfo info = ((AxisConfigurationImpl) sysContext
-					.getAxisConfiguration()).getPhasesinfo();
-			if (info != null) {
-				info.setOperationPhases(axisOperationTemplate);
-			}
-			axisService.addOperation(axisOperationTemplate);
-			sysContext.getAxisConfiguration().addService(axisService);
-			ServiceContext serviceContext = axisService.getParent()
-					.getServiceGroupContext(sysContext).getServiceContext(
-							assumedServiceName.getLocalPart());
+            ConfigurationContext configContext = null;
+            ConfigurationContextFactory efac = new ConfigurationContextFactory();
+            configContext = efac.buildClientConfigurationContext(null);
+            QName assumedServiceName = new QName("AnonymousService");
+            AxisService axisService = new AxisService(assumedServiceName);
+            AxisOperation axisOperationTemplate = new OutInAxisOperation(
+                    new QName("TemplateOperation"));
+            PhasesInfo info = ((AxisConfigurationImpl) configContext
+                    .getAxisConfiguration()).getPhasesinfo();
+            if (info != null) {
+                info.setOperationPhases(axisOperationTemplate);
+            }
+            axisService.addOperation(axisOperationTemplate);
+            configContext.getAxisConfiguration().addService(axisService);
+            ServiceContext serviceContext = axisService.getParent()
+                    .getServiceGroupContext(configContext).getServiceContext(
+                    assumedServiceName.getLocalPart());
 
-			MessageContext msgCtx = new MessageContext(serviceContext
-					.getConfigurationContext());
+            MessageContext msgCtx = new MessageContext(serviceContext
+                    .getConfigurationContext());
 
-			if (smc.getSoapAction() != null)
-				msgCtx.setSoapAction(smc.getSoapAction());
-			if (smc.getTo() != null)
-				msgCtx.setTo(smc.getTo());
-			if (smc.getFrom() != null)
-				msgCtx.setFrom(smc.getFrom());
-			if (smc.getMessageID() != null)
-				msgCtx.setMessageID(smc.getMessageID());
-			else
-				msgCtx.setMessageID(String.valueOf("uuid:"
-						+ UUIDGenerator.getUUID()));
-			if (smc.getReplyTo() != null)
-				msgCtx.setReplyTo(smc.getReplyTo());
-			if (smc.getRelatesTo() != null)
-				msgCtx.setRelatesTo(smc.getRelatesTo());
+            if (smc.getSoapAction() != null)
+                msgCtx.setSoapAction(smc.getSoapAction());
+            if (smc.getTo() != null)
+                msgCtx.setTo(smc.getTo());
+            if (smc.getWSAAction() != null)
+                msgCtx.setWSAAction(smc.getWSAAction());
+            if (smc.getFrom() != null)
+                msgCtx.setFrom(smc.getFrom());
+            if (smc.getMessageID() != null)
+                msgCtx.setMessageID(smc.getMessageID());
+            else
+                msgCtx.setMessageID(String.valueOf("uuid:"
+                        + UUIDGenerator.getUUID()));
+            if (smc.getReplyTo() != null)
+                msgCtx.setReplyTo(smc.getReplyTo());
+            if (smc.getRelatesTo() != null)
+                msgCtx.setRelatesTo(smc.getRelatesTo());
+            /**
+             * We need to detach the body of the Env and attach only the body
+             * part that is necessary
+             */
 
-			msgCtx.setEnvelope(smc.getEnvelope());
-			if (msgCtx.getEnvelope().getHeader() == null)
-				msgCtx.getEnvelope().getBody().insertSiblingBefore(
-						OMAbstractFactory.getSOAP11Factory()
-								.getDefaultEnvelope().getHeader());
+            //msgCtx.setEnvelope(smc.getEnvelope());
+            msgCtx.setEnvelope(outEnvelopeConfiguration(smc));
+            if (msgCtx.getEnvelope().getHeader() == null)
+                msgCtx.getEnvelope().getBody().insertSiblingBefore(
+                        OMAbstractFactory.getSOAP11Factory()
+                                .getDefaultEnvelope().getHeader());
 
-			msgCtx.setServiceContext(serviceContext);
+            msgCtx.setServiceContext(serviceContext);
 
-			EndpointReference epr = msgCtx.getTo();
-			String transport = null;
-			if (epr != null) {
-				String toURL = epr.getAddress();
-				int index = toURL.indexOf(':');
-				if (index > 0) {
-					transport = toURL.substring(0, index);
-				}
-			}
+            EndpointReference epr = msgCtx.getTo();
+            String transport = null;
+            if (epr != null) {
+                String toURL = epr.getAddress();
+                int index = toURL.indexOf(':');
+                if (index > 0) {
+                    transport = toURL.substring(0, index);
+                }
+            }
 
-			if (transport != null) {
+            if (transport != null) {
 
-				msgCtx.setTransportOut(serviceContext.getConfigurationContext()
-						.getAxisConfiguration().getTransportOut(
-								new QName(transport)));
+                msgCtx.setTransportOut(serviceContext.getConfigurationContext()
+                        .getAxisConfiguration().getTransportOut(
+                        new QName(transport)));
 
-			} else {
-				throw new SynapseException("cannotInferTransport");
-			}
-			// initialize and set the Operation Context
+            } else {
+                throw new SynapseException("cannotInferTransport");
+            }
+            // initialize and set the Operation Context
 
-			msgCtx.setOperationContext(axisOperationTemplate
-					.findOperationContext(msgCtx, serviceContext));
-			AxisEngine engine = new AxisEngine(sysContext);
-			engine.send(msgCtx);
+            msgCtx.setOperationContext(axisOperationTemplate
+                    .findOperationContext(msgCtx, serviceContext));
+            AxisEngine engine = new AxisEngine(configContext);
+            engine.send(msgCtx);
 
-			MessageContext response = new MessageContext(msgCtx
-					.getSystemContext(), msgCtx.getSessionContext(), msgCtx
-					.getTransportIn(), msgCtx.getTransportOut());
-			response.setProperty(MessageContext.TRANSPORT_IN, msgCtx
-					.getProperty(MessageContext.TRANSPORT_IN));
-			msgCtx.getAxisOperation().registerOperationContext(response,
-					msgCtx.getOperationContext());
-			response.setServerSide(false);
-			response.setServiceContext(msgCtx.getServiceContext());
-			response.setServiceGroupContext(msgCtx.getServiceGroupContext());
+            MessageContext response = new MessageContext(msgCtx
+                    .getSystemContext(), msgCtx.getSessionContext(), msgCtx
+                    .getTransportIn(), msgCtx.getTransportOut());
+            response.setProperty(MessageContext.TRANSPORT_IN, msgCtx
+                    .getProperty(MessageContext.TRANSPORT_IN));
+            msgCtx.getAxisOperation().registerOperationContext(response,
+                    msgCtx.getOperationContext());
+            response.setServerSide(false);
+            response.setServiceContext(msgCtx.getServiceContext());
+            response.setServiceGroupContext(msgCtx.getServiceGroupContext());
 
-			// If request is REST we assume the response is REST, so set the
-			// variable
-			response.setDoingREST(msgCtx.isDoingREST());
+            // If request is REST we assume the response is REST, so set the
+            // variable
+            response.setDoingREST(msgCtx.isDoingREST());
 
-			SOAPEnvelope resenvelope = TransportUtils.createSOAPMessage(
-					response, msgCtx.getEnvelope().getNamespace().getName());
+            SOAPEnvelope resenvelope = TransportUtils.createSOAPMessage(
+                    response, msgCtx.getEnvelope().getNamespace().getName());
 
-			response.setEnvelope(resenvelope);
-			engine = new AxisEngine(msgCtx.getSystemContext());
-			engine.receive(response);
-			response.setProperty(Constants.ISRESPONSE_PROPERTY, new Boolean(
-					true));
-			return response;
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new SynapseException(e);
-		}
+            response.setEnvelope(resenvelope);
+            engine = new AxisEngine(msgCtx.getSystemContext());
+            engine.receive(response);
+            response.setProperty(Constants.ISRESPONSE_PROPERTY, new Boolean(
+                    true));
+            return response;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new SynapseException(e);
+        }
 
-	}
+    }
+
+    private static SOAPEnvelope outEnvelopeConfiguration(MessageContext smc) {
+        SOAPEnvelope env = smc.getEnvelope();
+        env.getHeader().detach();
+        return env;
+    }
 
 }

bf721254579115e7b9aee252ea519486deb794ad
Saminda Wishwajith Abeyruwan <saminda@apache.org>
2005-12-10 23:04:12 +0000

    We have a test case for SendOn.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/synapse/trunk@355793 13f79535-47bb-0310-9956-ffa450edef68

--- java/test/org/apache/synapse/spi/injection/SendOnProcessorWithRuleTest.java
+++ java/test/org/apache/synapse/spi/injection/SendOnProcessorWithRuleTest.java
@@ -4,6 +4,7 @@ import junit.framework.TestCase;
 import org.apache.axis2.Constants;
 import org.apache.axis2.addressing.EndpointReference;
 import org.apache.axis2.client.MessageSender;
+import org.apache.axis2.client.Options;
 import org.apache.axis2.transport.http.SimpleHTTPServer;
 import org.apache.synapse.util.Axis2EvnSetup;
 
@@ -26,9 +27,7 @@ import javax.xml.namespace.QName;
 */
 
 public class SendOnProcessorWithRuleTest extends TestCase {
-    // todo: i have  to comment out this test case
-    // todo: because SimpleHTTPServer does not allow
-    // todo: to initiate multiple objects. :(
+    
     private SimpleHTTPServer synapseServer;
     private SimpleHTTPServer axis2Server;
     private EndpointReference targetEpr = new EndpointReference(
@@ -36,27 +35,28 @@ public class SendOnProcessorWithRuleTest extends TestCase {
     private QName operation = new QName("anonymous");
 
     public void setUp() throws Exception {
-//        synapseServer = new SimpleHTTPServer("target/synapse-repository-sendon",
-//                5043);
-//        /**
-//         * axis2Server is the one who holds the actual service
-//         */
-//        axis2Server = new SimpleHTTPServer("synapse-repository-sendonAxis2",
-//                8090);
-//        synapseServer.start();
-//        axis2Server.start();
+        synapseServer = new SimpleHTTPServer("target/synapse-repository-sendon",
+                5043);
+        /**
+         * axis2Server is the one who holds the actual service
+         */
+        axis2Server = new SimpleHTTPServer("synapse-repository-sendonAxis2",
+                8090);
+        synapseServer.start();
+        axis2Server.start();
     }
 
     protected void tearDown() throws Exception {
-//        synapseServer.stop();
-//        axis2Server.stop();
+        synapseServer.stop();
+        axis2Server.stop();
     }
 
     public void testSendPrcessor() throws Exception {
-//        MessageSender msgSender = new MessageSender();
-//        msgSender.setTo(targetEpr);
-//        msgSender.setSenderTransport(Constants.TRANSPORT_HTTP);
-//        msgSender.send(operation.getLocalPart(), Axis2EvnSetup.payload());
+        MessageSender msgSender = new MessageSender();
+        Options options = new Options();
+        options.setTo(targetEpr);
+        msgSender.setClientOptions(options);
+        msgSender.send(operation.getLocalPart(), Axis2EvnSetup.payload());
 
     }
 

bf0e3d760d21fe0d586bcf5bf9658e0bf89411a9
Saminda Wishwajith Abeyruwan <saminda@apache.org>
2005-12-13 06:07:56 +0000

    Modified code to cope with Axis2-0.94-SNAPSHOT AddressingInProcessorTest is excluded from test case due a problem in AddressingInHandler in Axis2. Once Axis2 folks get it fix, will include the test case
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/synapse/trunk@356484 13f79535-47bb-0310-9956-ffa450edef68

--- java/test/org/apache/synapse/TestSynapseEnvironment.java
+++ java/test/org/apache/synapse/TestSynapseEnvironment.java
@@ -2,7 +2,7 @@ package org.apache.synapse;
 
 import org.apache.synapse.axis2.Axis2SynapseEnvironment;
 
-public class SynapseEnvironmentTest {
+public class TestSynapseEnvironment {
 
 	public static SynapseEnvironment createAxis2SynapseEnvironment() {
 		Axis2SynapseEnvironment se = new Axis2SynapseEnvironment(null, null);

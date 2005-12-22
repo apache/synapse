4e95af7965cc4bdafd15c3dd7b6939dc5f7e24ff
Saminda Wishwajith Abeyruwan <saminda@apache.org>
2005-12-22 05:59:40 +0000

    1.Test cases have been updated to cope with the current Axis2 status.
    2. AxisFlexibleMEPClient and Axis2Sender logic modified.  Part of the code need to be modified later due dependencies 
    to Axis2 and its bug fixes. Temporary hacks has to be introduced till Axis2 fix the bugs.
     
    3.Added SecurtityProcessor and SecurityProcessorConfigurator as the first step towards giving QOS for Synapse. This 
    work is still under experimental stage. Till the stable code, security resources are put to the 
    synapse-test-resources.
     
    4.Temporary hack introduce to SynapseMessageReceiver to handle 200 OK and 202 OK. The logic behind this need to be 
    updated.
     
    5.As there has been so many improvements to Axis2-0.94 latest SNAPSHOT, the current state of the Synapse is not 
    stable and subject to change when some part of Axis2 changes. Sorry.
     
    6.Build is successful with these commits. 
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/synapse/trunk/java@358509 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/synapse/xml/ProcessorConfiguratorFinder.java
+++ src/org/apache/synapse/xml/ProcessorConfiguratorFinder.java
@@ -59,7 +59,7 @@ public class ProcessorConfiguratorFinder {
 			FaultProcessorConfigurator.class,
 			AddressingProcessorConfigurator.class,
 			InProcessorConfigurator.class, OutProcessorConfigurator.class,
-			NeverProcessorConfigurator.class, RefProcessorConfigurator.class, Axis2MediatorProcessorConfigurator.class };
+			NeverProcessorConfigurator.class, RefProcessorConfigurator.class};
 
 	private static void initialise() {
 

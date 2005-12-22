49a5749de7365bea4ac7ce643bbecf0ecff32d0f
Paul Fremantle <pzf@apache.org>
2005-12-22 13:50:17 +0000

    fix changes to axis2
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/synapse/trunk@358567 13f79535-47bb-0310-9956-ffa450edef68

--- java/src/samples/userguide/DumbStockQuoteClient.java
+++ java/src/samples/userguide/DumbStockQuoteClient.java
@@ -86,7 +86,7 @@ public class DumbStockQuoteClient {
 			AxisService as = new AxisService("AnonymousService");
 			asg.addService(as);
 			ServiceGroupContext sgc = new ServiceGroupContext(cc, asg);
-			ServiceContext sc = sgc.getServiceContext("AnonymousService");
+			ServiceContext sc = sgc.getServiceContext(as);
 			AxisOperation axisOperationTemplate = new OutInAxisOperation(
 					new QName("getQuote"));
 			as.addOperation(axisOperationTemplate);

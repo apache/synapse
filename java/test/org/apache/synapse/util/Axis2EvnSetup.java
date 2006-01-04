39849c1497ab461618010b3e1bf28d3c40f2b74c
Saminda Wishwajith Abeyruwan <saminda@apache.org>
2006-01-04 14:49:27 +0000

    Added test case to test mediation between HTTP and TCP transports. Corrected complie errors.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/synapse/trunk@365921 13f79535-47bb-0310-9956-ffa450edef68

--- java/test/org/apache/synapse/util/Axis2EvnSetup.java
+++ java/test/org/apache/synapse/util/Axis2EvnSetup.java
@@ -41,7 +41,7 @@ public class Axis2EvnSetup {
             throws AxisFault {
         ConfigurationContextFactory conFac = new ConfigurationContextFactory();
         ConfigurationContext configCtx = conFac
-                .buildConfigurationContext(testingRepository);
+                .createConfigurationContextFromFileSystem(testingRepository);
         MessageContext msgCtx = new MessageContext(configCtx);
         msgCtx.setEnvelope(testEnvSetup());
         msgCtx.setServerSide(true);

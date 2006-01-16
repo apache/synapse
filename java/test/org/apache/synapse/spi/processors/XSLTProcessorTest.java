02baf2d6589bcbc7a78699f258d4aab5654155b6
Saminda Wishwajith Abeyruwan <saminda@apache.org>
2006-01-16 05:26:01 +0000

    1. Added XSLTProcessor/Configuratior to core 2. Added XSLTProcessor test case 3. Added resources and updated maven.xml
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/synapse/trunk@369353 13f79535-47bb-0310-9956-ffa450edef68

--- java/test/org/apache/synapse/spi/processors/XSLTProcessorTest.java
+++ java/test/org/apache/synapse/spi/processors/XSLTProcessorTest.java
@@ -1,6 +1,12 @@
 package org.apache.synapse.spi.processors;
 
 import junit.framework.TestCase;
+import org.apache.synapse.SynapseMessage;
+import org.apache.synapse.processors.builtin.xslt.XSLTProcessor;
+import org.apache.synapse.util.Axis2EvnSetup;
+import org.apache.synapse.axis2.Axis2SynapseMessage;
+
+import java.io.ByteArrayInputStream;
 /*
 * Copyright 2004,2005 The Apache Software Foundation.
 *
@@ -19,7 +25,23 @@ import junit.framework.TestCase;
 */
 
 public class XSLTProcessorTest extends TestCase {
+    private String xsl = "<xsl:stylesheet version='1.0'\n" +
+            "                xmlns:xsl='http://www.w3.org/1999/XSL/Transform'>\n" +
+            "    <xsl:template match=\"/\">\n" +
+            "        <transformedText>\n" +
+            "            <xsl:value-of select=\"//text\"/>\n" +
+            "            <xsl:text>Test Being Transformed</xsl:text>" +
+            "        </transformedText>\n" +
+            "    </xsl:template>\n" +
+            "</xsl:stylesheet>";
+
     public void testXSLTProcessor() throws Exception {
-        //todo;
+        SynapseMessage sm = new Axis2SynapseMessage(
+                Axis2EvnSetup.axis2Deployment("target/synapse-repository"));
+        XSLTProcessor pro = new XSLTProcessor();
+        pro.setXSLInputStream(new ByteArrayInputStream(xsl.getBytes()));
+        pro.setIsBody(true);
+        boolean result = pro.process(null, sm);
+        assertTrue(result);
     }
 }

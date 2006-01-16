ee22c41521a768c6244409c1be56fa763819dfd9
P. Vikas Roonwal <vikas@apache.org>
2006-01-16 05:04:28 +0000

    somebody forgot the '}'
    git-svn-id: https://svn.apache.org/repos/asf/incubator/synapse/trunk@369347 13f79535-47bb-0310-9956-ffa450edef68

--- java/src/samples/userguide/DumbStockQuoteClient.java
+++ java/src/samples/userguide/DumbStockQuoteClient.java
@@ -1,105 +1,87 @@
 package samples.userguide;
 
 
-
-import java.net.URL;
-
-import javax.xml.namespace.QName;
-
 import org.apache.axis2.Constants;
 import org.apache.axis2.addressing.EndpointReference;
-
 import org.apache.axis2.client.Options;
 import org.apache.axis2.client.ServiceClient;
 import org.apache.axis2.context.ConfigurationContext;
-
-import org.apache.axis2.description.AxisOperation;
-import org.apache.axis2.description.AxisService;
-import org.apache.axis2.description.AxisServiceGroup;
-import org.apache.axis2.description.OutInAxisOperation;
-import org.apache.axis2.description.TransportOutDescription;
+import org.apache.axis2.description.*;
 import org.apache.axis2.engine.AxisConfiguration;
-
-import org.apache.axis2.om.OMAbstractFactory;
-import org.apache.axis2.om.OMElement;
-import org.apache.axis2.om.OMFactory;
-import org.apache.axis2.om.OMNamespace;
-import org.apache.axis2.om.OMText;
+import org.apache.axis2.om.*;
 import org.apache.axis2.transport.http.CommonsHTTPTransportSender;
 import org.apache.axis2.transport.http.HTTPConstants;
 import org.apache.axis2.transport.http.HttpTransportProperties;
 import org.apache.axis2.transport.http.HttpTransportProperties.ProxyProperties;
 
+import javax.xml.namespace.QName;
+
 public class DumbStockQuoteClient {
 
-	/**
-	 * 
-	 * <p>
-	 * This is a fairly static test client for Synapse. It makes a StockQuote
-	 * request to XMethods stockquote service. There is no EPR and there is no proxy config.
-	 * It's sort of a Gateway case. It relies on a Synapse config that will look at the URL or 
-	 * message and send it to the right place
-	 */
-	public static void main(String[] args) {
+    /**
+     * <p/>
+     * This is a fairly static test client for Synapse. It makes a StockQuote
+     * request to XMethods stockquote service. There is no EPR and there is no proxy config.
+     * It's sort of a Gateway case. It relies on a Synapse config that will look at the URL or
+     * message and send it to the right place
+     */
+    public static void main(String[] args) {
 
-		if (args.length > 0 && args[0].substring(0, 1).equals("-")) {
-			System.out
-					.println("This client demonstrates Synapse as a gateway\n"
-							+ "Usage: DumbStockQuoteClient Symbol SynapseURL");
-			System.out
-					.println("\nDefault values: IBM http://localhost:8080/StockQuote"
-							+ "\nAll examples depend on using the sample synapse.xml");
-			System.exit(0);
-		}
+        if (args.length > 0 && args[0].substring(0, 1).equals("-")) {
+            System.out
+                    .println("This client demonstrates Synapse as a gateway\n"
+                            + "Usage: DumbStockQuoteClient Symbol SynapseURL");
+            System.out
+                    .println("\nDefault values: IBM http://localhost:8080/StockQuote"
+                            + "\nAll examples depend on using the sample synapse.xml");
+            System.exit(0);
+        }
 
-		String symb = "IBM";
-		String url = "http://localhost:8080/StockQuote";
-		
+        String symb = "IBM";
+        String url = "http://localhost:8080/StockQuote";
 
-		if (args.length > 0)
-			symb = args[0];
-		if (args.length > 1)
-			url = args[1];
-		try {
 
-			// step 1 - create a request payload
-			OMElement getQuote = StockQuoteXMLHandler
-					.createRequestPayload(symb);
+        if (args.length > 0)
+            symb = args[0];
+        if (args.length > 1)
+            url = args[1];
+        try {
 
-			// step 2 - set up the call object
+            // step 1 - create a request payload
+            OMElement getQuote = StockQuoteXMLHandler
+                    .createRequestPayload(symb);
 
-			// the wsa:To
-			EndpointReference targetEPR = new EndpointReference(url);
+            // step 2 - set up the call object
 
-			Options options = new Options();
-			options.setTo(targetEPR);
+            // the wsa:To
+            EndpointReference targetEPR = new EndpointReference(url);
 
-			options.setSoapAction("http://www.webserviceX.NET/GetQuote");
-			// create a lightweight Axis Config with no addressing to
-			// demonstrate "dumb" SOAP
+            Options options = new Options();
+            options.setTo(targetEPR);
 
-			ServiceClient serviceClient = StockQuoteXMLHandler
-					.createServiceClient();
-			// make the ServiceClient
+            options.setSoapAction("http://www.webserviceX.NET/GetQuote");
+            // create a lightweight Axis Config with no addressing to
+            // demonstrate "dumb" SOAP
 
-			serviceClient.setOptions(options);
+            ServiceClient serviceClient = StockQuoteXMLHandler
+                    .createServiceClient();
+            // make the ServiceClient
 
-			// step 3 - Blocking invocation
-			OMElement result = serviceClient.sendReceive(new QName("getQuote"),
-					getQuote);
-			// System.out.println(result);
+            serviceClient.setOptions(options);
 
-			// step 4 - parse result
+            // step 3 - Blocking invocation
+            OMElement result = serviceClient.sendReceive(new QName("getQuote"),
+                    getQuote);
+            // System.out.println(result);
 
-			System.out.println("Stock price = $"
-					+ StockQuoteXMLHandler.parseResponse(result));
+            // step 4 - parse result
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            System.out.println("Stock price = $"
+                    + StockQuoteXMLHandler.parseResponse(result));
 
-	}
-		
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+    }
 }

ca0f05de9c5bc2a51492177227daf47a91b5fe9f
P. Vikas Roonwal <vikas@apache.org>
2006-02-07 10:24:10 +0000

    The prototype of synapse object for property and parameter handling!
    git-svn-id: https://svn.apache.org/repos/asf/incubator/synapse/trunk@375561 13f79535-47bb-0310-9956-ffa450edef68

---
+++ scratch/infravio/synapse-SO/src/com/infravio/utils/UtilConstants.java
@@ -0,0 +1,9 @@
+package com.infravio.utils;
+
+public class UtilConstants {
+
+    public static String NODE = "SynapseObject";
+    public static String LEAF = "attribute";
+    public static String NAME_ATTRIB = "name";
+    public static String TYPE_ATTRIB = "type";
+}

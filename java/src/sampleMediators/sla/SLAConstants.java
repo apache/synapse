d91827e71a62c38af66085fdf41851c54889ef6a
P. Vikas Roonwal <vikas@apache.org>
2006-02-13 12:15:13 +0000

    Checking in the modified SLAMediator by Soumadeep/Infravio
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/synapse/trunk@377363 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/src/sampleMediators/sla/SLAConstants.java
@@ -0,0 +1,10 @@
+
+package sampleMediators.sla;
+
+public interface SLAConstants {
+
+    public static final String CFG_SLA_ENABLED = "slaEnabled";
+    public static final String CFG_SLA_RESULT = "synapse.sla.result";
+    public static final String CFG_SLA_XML = "sla.xml";
+    public static final String CFG_XML_FOLDER = "META-INF";
+}

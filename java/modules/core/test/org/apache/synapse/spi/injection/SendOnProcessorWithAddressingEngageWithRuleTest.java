82fa4a4557fc31bdd236131cb43279752fdf87bc
Saminda Wishwajith Abeyruwan <saminda@apache.org>
2006-04-06 16:32:01 +0000

    chages to cope with Axis2
    git-svn-id: https://svn.apache.org/repos/asf/incubator/synapse/trunk@392009 13f79535-47bb-0310-9956-ffa450edef68

--- java/modules/core/test/org/apache/synapse/spi/injection/SendOnProcessorWithAddressingEngageWithRuleTest.java
+++ java/modules/core/test/org/apache/synapse/spi/injection/SendOnProcessorWithAddressingEngageWithRuleTest.java
@@ -37,10 +37,10 @@ public class SendOnProcessorWithAddressingEngageWithRuleTest extends TestCase {
     public void setUp() throws Exception {
         ConfigurationContext synapseServerContext = ConfigurationContextFactory
                 .createConfigurationContextFromFileSystem(
-                        "target/synapse-repository-sendon", null);
+                        "target/synapse-repository-sendon", "target/synapse-repository-sendon/conf/axis2.xml");
         ConfigurationContext serverContext = ConfigurationContextFactory
                 .createConfigurationContextFromFileSystem(
-                        "target/synapse-repository-sendonAxis2", null);
+                        "target/synapse-repository-sendonAxis2", "target/synapse-repository-sendonAxis2/conf/axis2.xml");
         synapseServer = new SimpleHTTPServer(synapseServerContext,5043);
         /**
          * axis2Server is the one who holds the actual service

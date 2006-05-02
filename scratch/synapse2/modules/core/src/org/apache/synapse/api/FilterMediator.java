f7a99376ef39acda80fd92df20d07c9c50e71423
Saminda Wishwajith Abeyruwan <saminda@apache.org>
2006-05-02 05:13:29 +0000

    Perform initial cleanup of codebase and introduce SynapseConfiguration. This is WIP and is not yet ready..
    git-svn-id: https://svn.apache.org/repos/asf/incubator/synapse/trunk@398811 13f79535-47bb-0310-9956-ffa450edef68

--- scratch/synapse2/modules/core/src/org/apache/synapse/api/FilterMediator.java
+++ scratch/synapse2/modules/core/src/org/apache/synapse/api/FilterMediator.java
@@ -2,6 +2,10 @@ package org.apache.synapse.api;
 
 import org.apache.synapse.SynapseMessage;
 
+/**
+ * The filter mediator is a list mediator, which executes the given list of mediators
+ * if the specified condition is satisfied
+ */
 public interface FilterMediator extends ListMediator {
-	public boolean test(SynapseMessage sm);
+    public boolean test(SynapseMessage sm);
 }

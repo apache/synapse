33a8753d3993babba77ad9dc673b6fc37f17094c
Saminda Wishwajith Abeyruwan <saminda@apache.org>
2006-05-30 08:31:30 +0000

    1. Added JSON <--> XML support to Synapse. This may need more testing before going into production 2. Added SynapseJsonSender 3. Added testcase JsonMediatorTest 4. all class in org.apache.synapse.json are obtained from json.org.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/synapse/trunk/java@410216 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/extensions/src/org/apache/synapse/config/xml/JsonMediatorFactory.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2004,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.synapse.config.xml;
+
+import org.apache.synapse.api.Mediator;
+import org.apache.synapse.mediators.ext.json.JsonMediator;
+import org.apache.synapse.SynapseException;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMAttribute;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.xml.namespace.QName;
+
+
+public class JsonMediatorFactory implements MediatorFactory{
+
+    private static final Log log = LogFactory.getLog(JsonMediatorFactory.class);
+
+    private static final QName tagName = new QName(Constants.SYNAPSE_NAMESPACE+"/json", "json");
+
+    public Mediator createMediator(OMElement elem) {
+        JsonMediator jsonMediator = new JsonMediator();
+        OMAttribute direction = elem.getAttribute(new QName("direction"));
+        if (direction == null) {
+            handleException("JSON element doesnot contain 'direction' attribute.");
+        }else {
+            jsonMediator.setDirection(direction.getAttributeValue().trim());
+        }
+        return jsonMediator;
+    }
+
+    public QName getTagQName() {
+        return tagName;
+    }
+    private void handleException(String msg) {
+        log.error(msg);
+        throw new SynapseException(msg);
+    }
+}

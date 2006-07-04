29f64fd3c5fddfb5c487bb150bb9012683aacb04
Asankha Chamath Perera <asankha@apache.org>
2006-07-04 11:20:09 +0000

    streamline proxy services all samples are updated to use the invesbot endpoint
    git-svn-id: https://svn.apache.org/repos/asf/incubator/synapse/trunk@418980 13f79535-47bb-0310-9956-ffa450edef68

--- java/modules/core/src/org/apache/synapse/core/axis2/ProxyService.java
+++ java/modules/core/src/org/apache/synapse/core/axis2/ProxyService.java
@@ -15,10 +15,9 @@
 */
 package org.apache.synapse.core.axis2;
 
-import org.apache.axis2.description.WSDL11ToAxisServiceBuilder;
-import org.apache.axis2.description.AxisService;
-import org.apache.axis2.description.AxisOperation;
-import org.apache.axis2.description.PolicyInclude;
+import org.apache.axis2.description.*;
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.transport.njms.JMSConstants;
 import org.apache.synapse.SynapseException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -28,138 +27,148 @@ import org.apache.ws.policy.Policy;
 
 import java.net.URL;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.StringTokenizer;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 /**
- * <proxy name="string" type="wsdl|jms|rest" [description="string"]>
- *   <endpoint protocols="(http|https|jms)+|all" uri="uri">
- *   <target sequence="name" | endpoint="name"/>?
+ * <proxy name="string" [description="string"] [transports="(http|https|jms)+|all"]>
+ *   <target sequence="name" | endpoint="name"/>?   // default is main sequence
  *   <wsdl url="url">?
  *   <schema url="url">*
  *   <policy url="url">*
+ *   <property name="string" value="string"/>*
  * </proxy>
  */
 public class ProxyService {
 
     private static final Log log = LogFactory.getLog(ProxyService.class);
-
-    public static final int WSDL_TYPE   = 1;
-    public static final int JMS_TYPE    = 2;
-    public static final int REST_TYPE   = 3;
-
-    public static final String PROTO_ALL   = "all";
-
+    /** The proxy service name */
     private String name;
-    private int type = WSDL_TYPE;           // default
+    /** The proxy service description */
     private String description;
-
-    /** The endpoint protocols the proxy service should be available on. This should contain the
-     * value 'all' to enable this service on all available transports (i.e. default) or should
-     * specify a comma seperated list of Axis2 transports
-     */
-    private String endpointProtocols = PROTO_ALL; // default
-    /** The endpoint URI where the proxy service should be available on */
-    private String endpointURI = null;
-
+    /** The transport/s over which this service should be exposed */
+    private String transports;
     /** The target endpoint, if assigned */
     private String targetEndpoint = null;
     /** The target sequence, if assigned */
     private String targetSequence = null;
-    // if a target endpoint or sequence is not specified, the default Synapse main mediator will be used
+    // if a target endpoint or sequence is not specified,
+    // the default Synapse main mediator will be used
+    /** A list properties */
+    private Map properties = new HashMap();
 
-    /** The URL for the base WSDL */
+    /** The URL for the base WSDL, if specified */
     private URL wsdl;
     /** The URLs for any supplied schemas */
     private URL[] schemas;
     /** The URLs for any supplied policies that would apply at the service level */
     private List serviceLevelPolicies = new ArrayList();
 
+    public static final String ALL_TRANSPORTS = "all";
+
     public ProxyService() {}
 
     public AxisService buildAxisService() {
-        if (type == ProxyService.WSDL_TYPE) {
+
+        AxisService proxyService = null;
+        if (wsdl != null) {
             try {
-                if (wsdl == null) {
-                    handleException("A WSDL URL is required for a WSDL based proxy service");
-                } else if (name == null) {
-                    handleException("A name is required for a proxy service");
-                } else if (endpointURI == null) {
-                    handleException("The endpoint URI for the proxy service has not been specified");
-                } else {
-
-                    WSDL11ToAxisServiceBuilder wsdl2AxisServiceBuilder =
-                        new WSDL11ToAxisServiceBuilder(wsdl.openStream(), null, null);
-                    AxisService proxyService = wsdl2AxisServiceBuilder.populateService();
-                    proxyService.setWsdlfound(true);
-
-                    // Set the name and description. Currently Axis2 uses the name as the Service name/URI
-                    proxyService.setName(name);
-                    proxyService.setServiceDescription(description);
-
-                    // TODO
-                    // Axis still does not allow us to set a pre-defined URL for a deployed service
-                    // proxyService.setEndpointURI(endpointURI);
-
-                    // set exposed transports http|https|jms|all.. etc
-                    if (!PROTO_ALL.equals(endpointProtocols)) {
-                        StringTokenizer st = new StringTokenizer(endpointProtocols, ",");
-                        String[] transposrts = new String[st.countTokens()];
-                        for (int i=0; i<transposrts.length; i++) {
-                            transposrts[i] = st.nextToken();
-                        }
-                        proxyService.setExposeTransports(transposrts);
-                    }
-
-                    // if service level policies are specified, apply them
-                    if (!serviceLevelPolicies.isEmpty()) {
-                        PolicyReader reader = PolicyFactory.getPolicyReader(PolicyFactory.OM_POLICY_READER);
-                        Policy svcEffectivePolicy = null;
-
-                        Iterator iter = serviceLevelPolicies.iterator();
-                        while (iter.hasNext()) {
-                            URL policyUrl = (URL) iter.next();
-                            if (svcEffectivePolicy == null) {
-                                svcEffectivePolicy = reader.readPolicy(policyUrl.openStream());
-                            } else {
-                                svcEffectivePolicy.merge(reader.readPolicy(policyUrl.openStream()));
-                            }
-                        }
-
-                        PolicyInclude policyInclude = new PolicyInclude();
-                        policyInclude.addPolicyElement(PolicyInclude.SERVICE_POLICY, svcEffectivePolicy);
-                        proxyService.setPolicyInclude(policyInclude);
-                    }
-
-                    // create a custom message receiver for this proxy service to use a given named
-                    // endpoint or sequence for forwarding/message mediation
-                    ProxyServiceMessageReceiver msgRcvr = new ProxyServiceMessageReceiver();
-                    msgRcvr.setName(name);
-                    if (targetEndpoint != null) {
-                        msgRcvr.setTargetEndpoint(targetEndpoint);
-                    } else if (targetSequence != null) {
-                        msgRcvr.setTargetSequence(targetSequence);
-                    }
-
-                    Iterator iter = proxyService.getOperations();
-                    while (iter.hasNext()) {
-                        AxisOperation op = (AxisOperation) iter.next();
-                        op.setMessageReceiver(msgRcvr);
-                    }
-
-                    return proxyService;
-                }
-
-            } catch (IOException e) {
-                handleException("Error opening input stream to WSDL at URL : " + wsdl, e);
+                WSDL11ToAxisServiceBuilder wsdl2AxisServiceBuilder =
+                    new WSDL11ToAxisServiceBuilder(wsdl.openStream(), null, null);
+                proxyService = wsdl2AxisServiceBuilder.populateService();
+                proxyService.setWsdlfound(true);
+            } catch (AxisFault af) {
+                handleException("Error building service from WSDL at : " + wsdl, af);
+            } catch (IOException ioe) {
+                handleException("Error reading WSDL from URL : " + wsdl, ioe);
             }
         } else {
-            // TODO
-            throw new UnsupportedOperationException("Only WSDL Proxy services are supported");
+            proxyService = new AxisService();
         }
-        return null;
+
+        // Set the name and description. Currently Axis2 uses the name as the
+        // default Service destination
+        proxyService.setName(name);
+        if (description != null) {
+            proxyService.setServiceDescription(description);
+        }
+
+        // process transports and expose over requested transports. If none
+        // is specified, default to all transports using service name as
+        // destination
+        if (transports == null || ALL_TRANSPORTS.equals(transports)) {
+            // default to all transports using service name as destination
+        } else {
+            StringTokenizer st = new StringTokenizer(transports, ", ");
+            String[] transports = new String[st.countTokens()];
+            for (int i=0; i<transports.length; i++) {
+                transports[i] = st.nextToken();
+            }
+            proxyService.setExposeTransports(transports);
+        }
+
+        // process parameters
+        Iterator iter = properties.keySet().iterator();
+        while (iter.hasNext()) {
+            String name  = (String) iter.next();
+            String value = (String) properties.get(name);
+            if (JMSConstants.CONFAC_PARAM.equals(name) ||
+                JMSConstants.DEST_PARAM.equals(name)) {
+
+                Parameter p = new Parameter();
+                p.setName(name);
+                p.setValue(value);
+
+                try {
+                    proxyService.addParameter(p);
+                } catch (AxisFault af) {
+                    handleException("Error setting property : " + name + "" +
+                        "to proxy service as a Parameter", af);
+                }
+            }
+        }
+
+        // if service level policies are specified, apply them
+        if (!serviceLevelPolicies.isEmpty()) {
+            PolicyReader reader = PolicyFactory.getPolicyReader(PolicyFactory.OM_POLICY_READER);
+            Policy svcEffectivePolicy = null;
+
+            URL policyUrl = null;
+            try {
+                iter = serviceLevelPolicies.iterator();
+                while (iter.hasNext()) {
+                    policyUrl = (URL) iter.next();
+                    if (svcEffectivePolicy == null) {
+                        svcEffectivePolicy = reader.readPolicy(policyUrl.openStream());
+                    } else {
+                        svcEffectivePolicy.merge(reader.readPolicy(policyUrl.openStream()));
+                    }
+                }
+            } catch (IOException ioe) {
+                handleException("Error reading policy from URL : " + policyUrl, ioe);
+            }
+
+            PolicyInclude policyInclude = new PolicyInclude();
+            policyInclude.addPolicyElement(PolicyInclude.SERVICE_POLICY, svcEffectivePolicy);
+            proxyService.setPolicyInclude(policyInclude);
+        }
+
+        // create a custom message receiver for this proxy service to use a given named
+        // endpoint or sequence for forwarding/message mediation
+        ProxyServiceMessageReceiver msgRcvr = new ProxyServiceMessageReceiver();
+        msgRcvr.setName(name);
+        if (targetEndpoint != null) {
+            msgRcvr.setTargetEndpoint(targetEndpoint);
+        } else if (targetSequence != null) {
+            msgRcvr.setTargetSequence(targetSequence);
+        }
+
+        iter = proxyService.getOperations();
+        while (iter.hasNext()) {
+            AxisOperation op = (AxisOperation) iter.next();
+            op.setMessageReceiver(msgRcvr);
+        }
+
+        return proxyService;
     }
 
     public String getName() {
@@ -170,14 +179,6 @@ public class ProxyService {
         this.name = name;
     }
 
-    public int getType() {
-        return type;
-    }
-
-    public void setType(int type) {
-        this.type = type;
-    }
-
     public String getDescription() {
         return description;
     }
@@ -186,20 +187,16 @@ public class ProxyService {
         this.description = description;
     }
 
-    public String getEndpointProtocols() {
-        return endpointProtocols;
+    public String getTransports() {
+        return transports;
     }
 
-    public void setEndpointProtocols(String endpointProtocols) {
-        this.endpointProtocols = endpointProtocols;
+    public void addProperty(String name, String value) {
+        properties.put(name, value);
     }
 
-    public String getEndpointURI() {
-        return endpointURI;
-    }
-
-    public void setEndpointURI(String endpointURI) {
-        this.endpointURI = endpointURI;
+    public void setTransports(String transports) {
+        this.transports = transports;
     }
 
     public String getTargetEndpoint() {
--- java/modules/samples/src/samples/userguide/DumbStockQuoteClient.java
+++ java/modules/samples/src/samples/userguide/DumbStockQuoteClient.java
@@ -1,86 +1,60 @@
+/*
+* Copyright 2004,2005 The Apache Software Foundation.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package samples.userguide;
 
-
-
-import org.apache.axis2.context.ConfigurationContextFactory;
-import org.apache.axis2.context.ConfigurationContext;
-import org.apache.axis2.addressing.EndpointReference;
-
+import org.apache.axiom.om.OMElement;
 import org.apache.axis2.client.Options;
 import org.apache.axis2.client.ServiceClient;
-import org.apache.axiom.om.OMElement;
+import org.apache.axis2.context.MessageContextConstants;
+import samples.common.InvesbotHandler;
 
+/**
+ * A simple example showing a dumb client.. the client just sets the transport
+ * to a Synapse server, which will determine and use the correct EPR for the
+ * message and send out.
+ */
 public class DumbStockQuoteClient {
 
-    /**
-     * <p/>
-     * This is a fairly static test client for Synapse. It makes a StockQuote
-     * request to XMethods stockquote service. There is no EPR and there is no
-     * proxy config. It's sort of a Gateway case. It relies on a Synapse config
-     * that will look at the URL or message and send it to the right place
-     */
     public static void main(String[] args) {
 
-        if (args.length > 0 && args[0].substring(0, 1).equals("-")) {
-            System.out
-                    .println("This client demonstrates Synapse as a gateway\n"
-                            + "Usage: DumbStockQuoteClient Symbol SynapseURL");
-            System.out
-                    .println(
-                            "\nDefault values: IBM http://localhost:8080/StockQuote"
-                                    +
-                                    "\nAll examples depend on using the sample synapse.xml");
-            System.exit(0);
-        }
+        String symbol = "IBM";
+        String turl = "http://localhost:8080/StockQuote";
+        String sAction = "http://ws.invesbot.com/GetQuote";
 
-        String symb = "IBM";
-        String url = "http://localhost:8080/StockQuote";
+        if (args.length > 0) symbol = args[0];
+        if (args.length > 1) turl = args[1];
 
-        if (args.length > 0)
-            symb = args[0];
-        if (args.length > 1)
-            url = args[1];
-        boolean repository = false;
-        if (args.length > 2) repository = true;
         try {
-            ServiceClient serviceClient;
-            if (repository) {
-
-                ConfigurationContext configContext =
-                        ConfigurationContextFactory.createConfigurationContextFromFileSystem(args[2],null);
-                serviceClient = new ServiceClient(configContext, null);
-            } else {
-                serviceClient = new ServiceClient();
-            }
-
-            // step 1 - create a request payload
-            OMElement getQuote = StockQuoteXMLHandler
-                    .createRequestPayload(symb);
-
-            // step 2 - set up the call object
-
-            // the wsa:To
-            EndpointReference targetEPR = new EndpointReference(url);
+            OMElement getQuote = InvesbotHandler.createStandardRequestPayload(symbol);
 
             Options options = new Options();
-            options.setTo(targetEPR);
+            if (turl != null)
+                options.setProperty(MessageContextConstants.TRANSPORT_URL, turl);
+            options.setAction(sAction);
 
-            options.setAction("http://www.webserviceX.NET/GetQuote");
-                        
+            ServiceClient serviceClient = new ServiceClient();
             serviceClient.setOptions(options);
 
-            // step 3 - Blocking invocation
-            OMElement result = serviceClient.sendReceive(getQuote);
-            // System.out.println(result);
-
-            // step 4 - parse result
-            System.out.println("Stock price = $"
-                    + StockQuoteXMLHandler.parseResponse(result));
+            OMElement result = serviceClient.sendReceive(getQuote).getFirstElement();
+            System.out.println("Standard :: Stock price = $" +
+                InvesbotHandler.parseStandardResponsePayload(result));
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
 }

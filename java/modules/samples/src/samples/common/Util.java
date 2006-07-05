2662f1659f1c58098efa472fe59c00e0b16f9bc6
Asankha Chamath Perera <asankha@apache.org>
2006-07-05 02:35:24 +0000

    add files which I have missed in the last commit
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/synapse/trunk@419139 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/modules/samples/src/samples/common/Util.java
@@ -0,0 +1,129 @@
+/*
+* Copyright 2004,2005 The Apache Software Foundation.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package samples.common;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.axis2.client.Options;
+import org.apache.axis2.client.ServiceClient;
+import org.apache.axis2.addressing.EndpointReference;
+import org.apache.axis2.context.MessageContextConstants;
+import org.apache.axis2.AxisFault;
+
+public class Util {
+
+    public static void testStandardQuote(
+        String symbol, String soapAction, String xurl, String turl) {
+
+        try {
+            OMElement getQuote = InvesbotHandler.createStandardRequestPayload(symbol);
+
+            Options options = new Options();
+            if (xurl != null)
+                options.setTo(new EndpointReference(xurl));
+            if (turl != null)
+                options.setProperty(MessageContextConstants.TRANSPORT_URL, turl);
+            options.setAction(soapAction);
+
+            ServiceClient serviceClient = new ServiceClient();
+            serviceClient.setOptions(options);
+
+            OMElement result = serviceClient.sendReceive(getQuote).getFirstElement();
+            System.out.println("Standard :: Stock price = $" +
+                InvesbotHandler.parseStandardResponsePayload(result));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void testCustomQuote(
+        String symbol, String soapAction, String xurl, String turl) {
+
+        try {
+            OMElement getQuote = InvesbotHandler.createCustomRequestPayload(symbol);
+
+            Options options = new Options();
+            if (xurl != null)
+                options.setTo(new EndpointReference(xurl));
+            if (turl != null)
+                options.setProperty(MessageContextConstants.TRANSPORT_URL, turl);
+            options.setAction(soapAction);
+
+            ServiceClient serviceClient = new ServiceClient();
+            serviceClient.setOptions(options);
+
+            OMElement result = serviceClient.sendReceive(getQuote).getFirstElement();
+            System.out.println("Custom :: Stock price = $" +
+                InvesbotHandler.parseCustomResponsePayload(result));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void testAdvancedQuote(
+        String symbol, String soapAction, String xurl, String turl) {
+        try {
+            OMElement getQuote = InvesbotHandler.createCustomRequestPayload(symbol);
+
+            Options options = new Options();
+            if (xurl != null)
+                options.setTo(new EndpointReference(xurl));
+            if (turl != null)
+                options.setProperty(MessageContextConstants.TRANSPORT_URL, turl);
+            options.setAction(soapAction);
+
+            ServiceClient serviceClient = new ServiceClient();
+            serviceClient.setOptions(options);
+
+            OMElement result = serviceClient.sendReceive(getQuote);
+            System.out.println("Custom :: Stock price = $" +
+                InvesbotHandler.parseCustomResponsePayload(result.getFirstElement()));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void testErroneousQuote(
+        String symbol, String soapAction, String xurl, String turl) {
+        try {
+            OMElement getQuote = InvesbotHandler.createErroneousCustomRequestPayload(symbol);
+
+            Options options = new Options();
+            if (xurl != null)
+                options.setTo(new EndpointReference(xurl));
+            if (turl != null)
+                options.setProperty(MessageContextConstants.TRANSPORT_URL, turl);
+            options.setAction(soapAction);
+
+            ServiceClient serviceClient = new ServiceClient();
+            serviceClient.setOptions(options);
+
+            OMElement result = serviceClient.sendReceive(getQuote);
+            System.out.println("Error :: Stock price = $" +
+                InvesbotHandler.parseCustomResponsePayload(result.getFirstElement()));
+
+        } catch (Exception e) {
+            if (e instanceof AxisFault) {
+                System.out.println("Fault : " + ((AxisFault)e).getFaultElements());
+            } else {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ java/modules/samples/src/samples/proxy/JMSProxySend.java
@@ -0,0 +1,36 @@
+/*
+* Copyright 2004,2005 The Apache Software Foundation.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package samples.proxy;
+
+import org.apache.axiom.om.OMFactory;
+import samples.common.Util;
+
+public class JMSProxySend {
+
+    public static void main(String[] args) {
+        String symbol  = "IBM";
+        String xurl    = "http://ws.invesbot.com/stockquotes.asmx";
+        String turl    = "jms:/JMSInvesbotSequenceProxy?transport.jms.ConnectionFactoryJNDIName=QueueConnectionFactory&java.naming.factory.initial=org.apache.activemq.jndi.ActiveMQInitialContextFactory&java.naming.provider.url=tcp://localhost:61616";
+        String sAction = "http://ws.invesbot.com/GetQuote";
+
+        if (args.length > 0) symbol = args[0];
+        if (args.length > 1) xurl   = args[1];
+        if (args.length > 2) turl   = args[2];
+
+        Util.testStandardQuote(symbol, sAction, xurl, turl);
+    }
+
+}

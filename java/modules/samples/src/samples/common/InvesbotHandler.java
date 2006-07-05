b8045323f67d8f17b34af77cbf6e05d3d1ea2776
Asankha Chamath Perera <asankha@apache.org>
2006-07-05 02:35:24 +0000

    add files which I have missed in the last commit
    git-svn-id: https://svn.apache.org/repos/asf/incubator/synapse/trunk@419139 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/modules/samples/src/samples/common/InvesbotHandler.java
@@ -0,0 +1,142 @@
+/*
+* Copyright 2004,2005 The Apache Software Foundation.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package samples.common;
+
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMNamespace;
+
+import javax.xml.namespace.QName;
+
+/**
+ * A class that can create messages to, and parse replies from the
+ * invesbot stock quote service at http://ws.invesbot.com/stockquotes.asmx
+ */
+public class InvesbotHandler {
+    /**
+     * Create a new custom stock quote request with a body as follows
+     * <m0:CheckPriceRequest xmlns:m0="http://www.apache-synapse.org/test">
+     *   <m0:Code>symbol</m0:Code>
+     * </m0:CheckPriceRequest>
+     * @param symbol the stock symbol
+     * @return OMElement for SOAP body
+     */
+    public static OMElement createCustomRequestPayload(String symbol) {
+        OMFactory factory   = OMAbstractFactory.getOMFactory();
+        OMNamespace ns      = factory.createOMNamespace(
+            "http://www.apache-synapse.org/test", "m0");
+        OMElement chkPrice  = factory.createOMElement("CheckPriceRequest", ns);
+        OMElement code      = factory.createOMElement("Code", ns);
+        chkPrice.addChild(code);
+        code.setText(symbol);
+        return chkPrice;
+    }
+
+    /**
+     * Create a new erroneous custom stock quote request with a body as follows
+     * <m0:CheckPriceRequest xmlns:m0="http://www.apache-synapse.org/test">
+     *   <m0:Symbol>symbol</m0:Symbol>
+     * </m0:CheckPriceRequest>
+     * @param symbol the stock symbol
+     * @return OMElement for SOAP body
+     */
+    public static OMElement createErroneousCustomRequestPayload(String symbol) {
+        OMFactory factory   = OMAbstractFactory.getOMFactory();
+        OMNamespace ns      = factory.createOMNamespace(
+            "http://www.apache-synapse.org/test", "m0");
+        OMElement chkPrice  = factory.createOMElement("CheckPriceRequest", ns);
+        OMElement code      = factory.createOMElement("Symbol", ns);
+        chkPrice.addChild(code);
+        code.setText(symbol);
+        return chkPrice;
+    }
+
+    /**
+     * Create a new custom stock quote request with a body as follows
+     *  <m:GetQuote xmlns:m="http://ws.invesbot.com/">
+     *      <m:symbol>IBM</m:symbol>
+     *  </m:GetQuote>
+     * @param symbol the stock symbol
+     * @return OMElement for SOAP body
+     */
+    public static OMElement createStandardRequestPayload(String symbol) {
+        OMFactory factory   = OMAbstractFactory.getOMFactory();
+        OMNamespace ns      = factory.createOMNamespace("http://ws.invesbot.com/", "m0");
+        OMElement getQuote  = factory.createOMElement("GetQuote", ns);
+        OMElement symb      = factory.createOMElement("symbol", ns);
+        getQuote.addChild(symb);
+        symb.setText(symbol);
+        return getQuote;
+    }
+
+    /**
+     * Digests the standard StockQuote response and extracts the last trade price
+     * @param result
+     * @return
+     * @throws javax.xml.stream.XMLStreamException
+     *
+     *  <GetQuoteResponse xmlns="http://ws.invesbot.com/">
+     *      <GetQuoteResult>
+     *          <StockQuote xmlns="">
+     *              <Symbol>IBM</Symbol>
+     *              ...
+     *              <Price>82.47</Price>
+     *              .......
+     *          </StockQuote>
+     *      </GetQuoteResult>
+     *  </GetQuoteResponse>
+     */
+    public static String parseStandardResponsePayload(OMElement result) throws Exception {
+
+        OMElement getQResp = result.getFirstChildWithName(
+            new QName("http://ws.invesbot.com/", "StockQuote"));
+        if (getQResp != null) {
+            OMElement price = getQResp.getFirstChildWithName(
+                new QName("http://ws.invesbot.com/", "Price"));
+            return price.getText();
+        } else {
+            throw new Exception("Unexpected response : " + result);
+        }
+    }
+
+    /**
+     * Digests the custom quote response and extracts the last trade price
+     * @param result
+     * @return
+     * @throws javax.xml.stream.XMLStreamException
+     *
+     *  <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+     *  <soap:Body>
+     *      <CheckPriceResponse xmlns="http://ws.invesbot.com/" >
+     *          <Code>IBM</Code>
+     *          <Price>82.90</Price>
+     *      </CheckPriceResponse>
+     *  </soap:Body>
+     *  </soap:Envelope>
+     */
+    public static String parseCustomResponsePayload(OMElement result) throws Exception {
+
+        OMElement chkPResp = result.getFirstChildWithName(
+            new QName("http://www.apache-synapse.org/test", "CheckPriceResponse"));
+        if (chkPResp != null) {
+            OMElement price = chkPResp.getFirstChildWithName(new QName("http://www.apache-synapse.org/test", "Price"));
+            return price.getText();
+        } else {
+            throw new Exception("Unexpected response : " + result);
+        }
+    }
+}

e488052416bb837ed97dfb6f622968c666649116
Saminda Wishwajith Abeyruwan <saminda@apache.org>
2006-07-12 08:58:15 +0000

    cheked in
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/synapse/trunk/java@421194 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/core/src/org/apache/synapse/config/xml/RMMediatorFactory.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2004,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.synapse.config.xml;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.synapse.api.Mediator;
+import org.apache.synapse.mediators.builtin.RMMediator;
+
+import javax.xml.namespace.QName;
+/*
+ * 
+ */
+
+public class RMMediatorFactory extends AbstractMediatorFactory {
+
+    private static Log log = LogFactory.getLog(RMMediatorFactory.class);
+
+    private static final QName RM_Q    = new QName(Constants.SYNAPSE_NAMESPACE, "enableRM");
+
+    public Mediator createMediator(OMElement elem) {
+        log.info("RMMediatorFactory  :: createMediator()");
+        //TODO: Fill properties if needed
+        return new RMMediator();
+    }
+
+    public QName getTagQName() {
+        return RM_Q;
+    }
+}

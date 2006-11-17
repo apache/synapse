0ef4b22cdb3f2a0912a33960d236e3334a796419
Asankha Chamath Perera <asankha@apache.org>
2006-11-17 18:24:52 +0000

    fix .bat/.sh and jar/mar
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/synapse/trunk@476238 13f79535-47bb-0310-9956-ffa450edef68

--- java/modules/samples/src/main/scripts/custom.bat
+++ java/modules/samples/src/main/scripts/custom.bat
@@ -1,18 +1,21 @@
 @echo off
 
-REM  Copyright 2001,2004-2005 The Apache Software Foundation
+REM   Licensed to the Apache Software Foundation (ASF) under one
+REM   or more contributor license agreements.  See the NOTICE file
+REM   distributed with this work for additional information
+REM   regarding copyright ownership.  The ASF licenses this file
+REM   to you under the Apache License, Version 2.0 (the
+REM   "License"); you may not use this file except in compliance
+REM   with the License.  You may obtain a copy of the License at
 REM
-REM  Licensed under the Apache License, Version 2.0 (the "License");
-REM  you may not use this file except in compliance with the License.
-REM  You may obtain a copy of the License at
+REM    http://www.apache.org/licenses/LICENSE-2.0
 REM
-REM      http://www.apache.org/licenses/LICENSE-2.0
-REM
-REM  Unless required by applicable law or agreed to in writing, software
-REM  distributed under the License is distributed on an "AS IS" BASIS,
-REM  WITHOUT WARRSYNAPSEIES OR CONDITIONS OF ANY KIND, either express or implied.
-REM  See the License for the specific language governing permissions and
-REM  limitations under the License.
+REM   Unless required by applicable law or agreed to in writing,
+REM   software distributed under the License is distributed on an
+REM   REM  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+REM   KIND, either express or implied.  See the License for the
+REM   specific language governing permissions and limitations
+REM   under the License.
 
 if "%OS%"=="Windows_NT" @setlocal
 if "%OS%"=="WINNT" @setlocal

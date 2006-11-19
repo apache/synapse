a87d7181bdf10621c5d0a94b108646545476c6d6
Asankha Chamath Perera <asankha@apache.org>
2006-11-19 19:04:40 +0000

    fix samples to demonstrate basic proxy services and an example using Rampart Includes updates to the documentation in the README.txt added a log4j properties to suppress unwanted console output during unit test execution
    git-svn-id: https://svn.apache.org/repos/asf/incubator/synapse/trunk@476886 13f79535-47bb-0310-9956-ffa450edef68

--- java/modules/samples/src/main/java/samples/userguide/StockQuoteClient.java
+++ java/modules/samples/src/main/java/samples/userguide/StockQuoteClient.java
@@ -20,12 +20,16 @@
 package samples.userguide;
 
 import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.impl.builder.StAXOMBuilder;
 import org.apache.axis2.Constants;
 import org.apache.axis2.addressing.EndpointReference;
 import org.apache.axis2.client.Options;
 import org.apache.axis2.client.ServiceClient;
 import org.apache.axis2.context.ConfigurationContext;
 import org.apache.axis2.context.ConfigurationContextFactory;
+import org.apache.rampart.RampartMessageData;
+import org.apache.neethi.PolicyEngine;
+import org.apache.neethi.Policy;
 import samples.common.StockQuoteHandler;
 
 import javax.xml.namespace.QName;
@@ -43,11 +47,13 @@ public class StockQuoteClient {
         String turl    = "http://localhost:8080";
         String sAction = "urn:getQuote";
         String repo    = "client_repo";
+        String secpol  = null;
 
         if (args.length > 0) symbol = args[0];
         if (args.length > 1) xurl   = args[1];
         if (args.length > 2) turl   = args[2];
         if (args.length > 3) repo   = args[3];
+        if (args.length > 4) secpol = args[4];
 
         try {
             OMElement getQuote = StockQuoteHandler.createStandardRequestPayload(symbol);
@@ -59,6 +65,11 @@ public class StockQuoteClient {
                 options.setProperty(Constants.Configuration.TRANSPORT_URL, turl);
             options.setAction(sAction);
 
+            if (secpol != null && secpol.length() > 0) {
+                options.setProperty(
+                    RampartMessageData.KEY_RAMPART_POLICY, loadPolicy(secpol));
+            }
+
             ServiceClient serviceClient = null;
             if (repo != null) {
                 ConfigurationContext configContext =
@@ -69,6 +80,10 @@ public class StockQuoteClient {
                 serviceClient = new ServiceClient();
             }
             serviceClient.engageModule(new QName("addressing"));
+            if (secpol != null && secpol.length() > 0) {
+                serviceClient.engageModule(new QName("rampart"));
+            }
+
             serviceClient.setOptions(options);
 
             OMElement result = serviceClient.sendReceive(getQuote).getFirstElement();
@@ -78,6 +93,11 @@ public class StockQuoteClient {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static Policy loadPolicy(String xmlPath) throws Exception {
+        StAXOMBuilder builder = new StAXOMBuilder(xmlPath);
+        return PolicyEngine.getPolicy(builder.getDocumentElement());
     }
 
 }

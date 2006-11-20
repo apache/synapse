efb96047b74d801a4026977ad9dc5ad46ecb9291
Asankha Chamath Perera <asankha@apache.org>
2006-11-20 13:30:03 +0000

    WS-Security example (example #3 of rampart) showing signed and encrypted messages received at a proxy service and then dispatched to a non-WS-Sec endpoint after stripping the wsse:Security header
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/synapse/trunk/java@477169 13f79535-47bb-0310-9956-ffa450edef68

--- modules/samples/src/main/java/samples/userguide/DumbStockQuoteClient.java
+++ modules/samples/src/main/java/samples/userguide/DumbStockQuoteClient.java
@@ -20,10 +20,18 @@
 package samples.userguide;
 
 import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.impl.builder.StAXOMBuilder;
 import org.apache.axis2.Constants;
+import org.apache.axis2.context.ConfigurationContext;
+import org.apache.axis2.context.ConfigurationContextFactory;
 import org.apache.axis2.client.Options;
 import org.apache.axis2.client.ServiceClient;
+import org.apache.rampart.RampartMessageData;
+import org.apache.neethi.Policy;
+import org.apache.neethi.PolicyEngine;
 import samples.common.StockQuoteHandler;
+
+import javax.xml.namespace.QName;
 
 /**
  * A simple example showing a dumb client.. the client just sets the transport
@@ -37,9 +45,13 @@ public class DumbStockQuoteClient {
         String symbol  = "IBM";
         String turl    = "http://localhost:9000/axis2/services/SimpleStockQuoteService";
         String sAction = "urn:getQuote";
+        String repo    = "client_repo";
+        String secpol  = null;
 
         if (args.length > 0) symbol = args[0];
-        if (args.length > 1) turl = args[1];
+        if (args.length > 1) turl   = args[1];
+        if (args.length > 2) repo   = args[2];
+        if (args.length > 3) secpol = args[3];
 
         try {
             OMElement getQuote = StockQuoteHandler.createStandardRequestPayload(symbol);
@@ -49,7 +61,24 @@ public class DumbStockQuoteClient {
                 options.setProperty(Constants.Configuration.TRANSPORT_URL, turl);
             options.setAction(sAction);
 
-            ServiceClient serviceClient = new ServiceClient();
+            if (secpol != null && secpol.length() > 0) {
+                options.setProperty(
+                    RampartMessageData.KEY_RAMPART_POLICY, loadPolicy(secpol));
+            }
+
+            ServiceClient serviceClient = null;
+            if (repo != null) {
+                ConfigurationContext configContext =
+                    ConfigurationContextFactory.
+                        createConfigurationContextFromFileSystem(repo, null);
+                serviceClient = new ServiceClient(configContext, null);
+            } else {
+                serviceClient = new ServiceClient();
+            }
+            
+            if (secpol != null && secpol.length() > 0) {
+                serviceClient.engageModule(new QName("rampart"));
+            }
             serviceClient.setOptions(options);
 
             OMElement result = serviceClient.sendReceive(getQuote).getFirstElement();
@@ -59,6 +88,11 @@ public class DumbStockQuoteClient {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static Policy loadPolicy(String xmlPath) throws Exception {
+        StAXOMBuilder builder = new StAXOMBuilder(xmlPath);
+        return PolicyEngine.getPolicy(builder.getDocumentElement());
     }
 
 }

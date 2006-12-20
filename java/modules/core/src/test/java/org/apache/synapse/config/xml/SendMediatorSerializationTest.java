217156155282c4e6cb2b3a0b9ba24a60f380843e
Asankha Chamath Perera <asankha@apache.org>
2006-12-20 06:50:14 +0000

    committing for Chathura for http://issues.apache.org/jira/browse/SYNAPSE-40
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/synapse/trunk@488932 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/modules/core/src/test/java/org/apache/synapse/config/xml/SendMediatorSerializationTest.java
@@ -0,0 +1,99 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *   * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.apache.synapse.config.xml;
+
+import org.apache.axiom.om.impl.exception.XMLComparisonException;
+
+public class SendMediatorSerializationTest extends AbstractTestCase {
+
+    private SendMediatorFactory sendMediatorFactory = null;
+    private SendMediatorSerializer sendMediatorSerializer = null;
+
+    public SendMediatorSerializationTest() {
+        sendMediatorFactory = new SendMediatorFactory();
+        sendMediatorSerializer = new SendMediatorSerializer();
+    }
+
+    public void testSendMediatorWithNoEndpoints() {
+
+        String sendConfiguration = "<syn:send xmlns:syn=\"http://ws.apache.org/ns/synapse\"/>";
+
+        try {
+            assertTrue(serialization(sendConfiguration, sendMediatorFactory, sendMediatorSerializer));
+        } catch (XMLComparisonException e) {
+            fail("Exception in test.");
+        }
+    }
+
+    public void testSendMediatorWithSingleEndpoint() {
+
+        String sendConfiguration = "<syn:send xmlns:syn=\"http://ws.apache.org/ns/synapse\">" +
+                "<syn:endpoint name=\"myendpoint\" address=\"http://ws.apache.org/axis2/services/Service1\" />" +
+                "</syn:send>";
+
+        try {
+            assertTrue(serialization(sendConfiguration, sendMediatorFactory, sendMediatorSerializer));
+        } catch (XMLComparisonException e) {
+            fail("Exception in test.");
+        }
+    }
+
+    public void testSendMediatorWithMultipleEndpoints() {
+
+        String sendConfiguration = "<syn:send xmlns:syn=\"http://ws.apache.org/ns/synapse\">" +
+                "<syn:endpoint name=\"myendpoint1\" address=\"http://ws.apache.org/axis2/services/Service1\" />" +
+                "<syn:endpoint name=\"myendpoint2\" address=\"http://ws.apache.org/axis2/services/Service2\" />" +
+                "</syn:send>";
+
+        try {
+            assertTrue(serialization(sendConfiguration, sendMediatorFactory, sendMediatorSerializer));
+        } catch (XMLComparisonException e) {
+            fail("Exception in test.");
+        }
+    }
+
+    public void testSendMediatorWithSingleEndpointReference() {
+
+        String sendConfiguration = "<syn:send xmlns:syn=\"http://ws.apache.org/ns/synapse\">" +
+                "<syn:endpoint ref=\"ep1\"/>" +
+                "</syn:send>";
+
+        try {
+            assertTrue(serialization(sendConfiguration, sendMediatorFactory, sendMediatorSerializer));
+        } catch (XMLComparisonException e) {
+            fail("Exception in test.");
+        }
+    }
+
+    public void testSendMediatorWithMultipleEndpointReferences() {
+
+        String sendConfiguration = "<syn:send xmlns:syn=\"http://ws.apache.org/ns/synapse\">" +
+                "<syn:endpoint ref=\"ep1\"/>" +
+                "<syn:endpoint ref=\"ep2\"/>" +
+                "</syn:send>";
+
+        try {
+            assertTrue(serialization(sendConfiguration, sendMediatorFactory, sendMediatorSerializer));
+        } catch (XMLComparisonException e) {
+            fail("Exception in test.");
+        }
+    }
+
+}

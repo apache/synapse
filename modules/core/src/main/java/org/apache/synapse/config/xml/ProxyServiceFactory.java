77e53e6e0d1f73a5861c9ceb5e4a134c58490f29
Endera Linton <ruwan@apache.org>
2007-02-06 05:41:28 +0000

    code reformat in ProxyServiceFactory
    git-svn-id: https://svn.apache.org/repos/asf/webservices/synapse/trunk/java@503996 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core/src/main/java/org/apache/synapse/config/xml/ProxyServiceFactory.java
+++ modules/core/src/main/java/org/apache/synapse/config/xml/ProxyServiceFactory.java
@@ -57,18 +57,18 @@ public class ProxyServiceFactory {
             handleException("The 'name' attribute is required for a Proxy service definition");
         } else {
             proxy.setName(name.getAttributeValue());
-            OMAttribute statistics = elem.getAttribute(
-                    new QName(Constants.NULL_NAMESPACE, Constants.STATISTICS_ATTRIB_NAME));
-            if (statistics != null) {
-                String statisticsValue = statistics.getAttributeValue();
-                if (statisticsValue != null) {
-                    if (Constants.STATISTICS_ENABLE.equals(statisticsValue)) {
-                        proxy.setStatisticsEnable(org.apache.synapse.Constants.STATISTICS_ON);
-                    } else if (Constants.STATISTICS_DISABLE.equals(statisticsValue)) {
-                        proxy.setStatisticsEnable(org.apache.synapse.Constants.STATISTICS_OFF);
-                    }
-                }
+        }
 
+        OMAttribute statistics = elem.getAttribute(
+                    new QName(Constants.NULL_NAMESPACE, Constants.STATISTICS_ATTRIB_NAME));
+        if (statistics != null) {
+            String statisticsValue = statistics.getAttributeValue();
+            if (statisticsValue != null) {
+                if (Constants.STATISTICS_ENABLE.equals(statisticsValue)) {
+                    proxy.setStatisticsEnable(org.apache.synapse.Constants.STATISTICS_ON);
+                } else if (Constants.STATISTICS_DISABLE.equals(statisticsValue)) {
+                    proxy.setStatisticsEnable(org.apache.synapse.Constants.STATISTICS_OFF);
+                }
             }
         }
 

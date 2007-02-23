a5b942a11a23c4ee5573dcee47e8cc6ae2ee5960
Asankha Chamath Perera <asankha@apache.org>
2007-02-23 19:43:10 +0000

    include NIO transport at debug log level for log4j support remote debugging with -xdebug option to synapse.sh
    git-svn-id: https://svn.eu.apache.org/repos/asf/webservices/synapse/trunk@511069 13f79535-47bb-0310-9956-ffa450edef68

--- java/src/main/bin/synapse.sh
+++ java/src/main/bin/synapse.sh
@@ -105,12 +105,20 @@ SYNAPSE_ENDORSED=$SYNAPSE_HOME/lib/endorsed
 # synapse.xml
 SYNAPSE_XML=-Dsynapse.xml=$SYNAPSE_HOME/repository/conf/synapse.xml
 
+if [ "$1" = "-xdebug" ]; then
+  XDEBUG="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,address=8000"
+fi
+
 if [ "$1" = "-sample" ]; then
-SYNAPSE_XML=-Dsynapse.xml=$SYNAPSE_HOME/repository/conf/sample/synapse_sample_$2.xml
+  SYNAPSE_XML=-Dsynapse.xml=$SYNAPSE_HOME/repository/conf/sample/synapse_sample_$2.xml
+fi
+
+if [ "$3" = "-xdebug" ]; then
+  XDEBUG="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,address=8000"
 fi
 
 if [ "$3" = "-sample" ]; then
-SYNAPSE_XML=-Dsynapse.xml=$SYNAPSE_HOME/repository/conf/sample/synapse_sample_$4.xml
+  SYNAPSE_XML=-Dsynapse.xml=$SYNAPSE_HOME/repository/conf/sample/synapse_sample_$4.xml
 fi
 
 PORT="-Dport=8080"
@@ -126,4 +134,4 @@ echo "Using SYNAPSE_HOME:    $SYNAPSE_HOME"
 echo "Using JAVA_HOME:       $JAVA_HOME"
 echo "Using SYNAPSE_XML:     $SYNAPSE_XML"
 
-$JAVA_HOME/bin/java $PORT $SYNAPSE_XML -Daxis2.xml=$SYNAPSE_HOME/repository/conf/axis2.xml -Djava.endorsed.dirs=$SYNAPSE_ENDORSED -classpath $SYNAPSE_CLASSPATH org.apache.synapse.SynapseServer $SYNAPSE_HOME/repository
+$JAVA_HOME/bin/java $XDEBUG $PORT $SYNAPSE_XML -Daxis2.xml=$SYNAPSE_HOME/repository/conf/axis2.xml -Djava.endorsed.dirs=$SYNAPSE_ENDORSED -classpath $SYNAPSE_CLASSPATH org.apache.synapse.SynapseServer $SYNAPSE_HOME/repository

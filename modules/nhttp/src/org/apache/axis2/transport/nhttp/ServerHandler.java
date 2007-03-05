fbdbdf9ef130a830a7a81a7ac7b2cf95153ad669
Asankha Chamath Perera <asankha@apache.org>
2007-03-05 06:02:08 +0000

    update NIO transport to write back custom http headers set on the Axis2 message context and to make the remote party IP address available to the Axis2 MC for incoming requests
    git-svn-id: https://svn.apache.org/repos/asf/webservices/synapse/trunk/java@514559 13f79535-47bb-0310-9956-ffa450edef68

--- modules/nhttp/src/org/apache/axis2/transport/nhttp/ServerHandler.java
+++ modules/nhttp/src/org/apache/axis2/transport/nhttp/ServerHandler.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *  http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -160,6 +160,10 @@ public class ServerHandler implements NHttpServiceHandler {
         } catch (IOException e) {
             handleException("I/O Error : " + e.getMessage(), e, conn);
         }
+    }
+
+    public void responseReady(NHttpServerConnection conn) {
+        // New API method - should not require
     }
 
     /**

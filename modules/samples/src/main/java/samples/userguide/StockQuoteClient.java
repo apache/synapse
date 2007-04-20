59b2091baf549a64bbfcc9f2fb383222e8f0b128
Asankha Chamath Perera <asankha@apache.org>
2007-04-20 08:20:43 +0000

    streamlining samples - take 2 add samples for MTOM/SwA
    git-svn-id: https://svn.apache.org/repos/asf/webservices/synapse/trunk/java@530689 13f79535-47bb-0310-9956-ffa450edef68

--- modules/samples/src/main/java/samples/userguide/StockQuoteClient.java
+++ modules/samples/src/main/java/samples/userguide/StockQuoteClient.java
@@ -22,6 +22,8 @@ package samples.userguide;
 import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.impl.builder.StAXOMBuilder;
 import org.apache.axis2.Constants;
+import org.apache.axis2.transport.http.HttpTransportProperties;
+import org.apache.axis2.transport.http.HTTPConstants;
 import org.apache.axis2.addressing.EndpointReference;
 import org.apache.axis2.client.Options;
 import org.apache.axis2.client.ServiceClient;
@@ -32,46 +34,46 @@ import org.apache.neethi.PolicyEngine;
 import org.apache.neethi.Policy;
 import samples.common.StockQuoteHandler;
 
-import javax.xml.namespace.QName;
+import java.net.URL;
 
 /**
- * The EPR to the actual service is set, but the transport is set to
- * the Synapse url.
+ * ant stockquote
+ *     [-Dsymbol=<sym>] [-Drepository=<repo>] [-Dpolicy=<policy>]
+ *     [-Dmode=quote | customquote | fullquote | placeorder | marketactivity]
+ *     [-Daddressingurl=<url>] [-Dtransporturl=<url> | -Dproxyurl=<url>]
+ * 
+ * StockQuoteClient <symbol> <quote | customquote | fullquote | placeorder | marketactivity>
+ *      <addurl> <trpurl> <prxurl> <repo> <policy>
  */
 public class StockQuoteClient {
 
     public static void main(String[] args) {
 
-        String symbol  = "IBM";
-        String xurl    = "http://localhost:9000/axis2/services/SimpleStockQuoteService";
-        String turl    = "http://localhost:8080";
-        String sAction = "urn:getQuote";
-        String repo    = "client_repo";
-        String secpol  = null;
+        // defaults
+        String symbol    = "IBM";
+        String mode      = "quote";
+        String addUrl    = "http://localhost:9000/axis2/services/SimpleStockQuoteService";
+        String trpUrl    = "http://localhost:8080";
+        String prxUrl    = null;
+        String repo      = "client_repo";
+        String svcPolicy = null;
 
-        if (args.length > 0) symbol = args[0];
-        if (args.length > 1) xurl   = args[1];
-        if (args.length > 2) turl   = args[2];
-        if (args.length > 3) repo   = args[3];
-        if (args.length > 4) secpol = args[4];
+        if (args.length > 0) symbol    = args[0];
+        if (args.length > 1) mode      = args[1];
+        if (args.length > 2) addUrl    = args[2];
+        if (args.length > 3) trpUrl    = args[3];
+        if (args.length > 4) prxUrl    = args[4];
+        if (args.length > 5) repo      = args[5];
+        if (args.length > 6) svcPolicy = args[6];
+
+        double price = 0; int quantity = 0;
 
         try {
-            OMElement getQuote = StockQuoteHandler.createStandardRequestPayload(symbol);
-
             Options options = new Options();
-            if (xurl != null)
-                options.setTo(new EndpointReference(xurl));
-            if (turl != null)
-                options.setProperty(Constants.Configuration.TRANSPORT_URL, turl);
-            options.setAction(sAction);
-
-            if (secpol != null && secpol.length() > 0) {
-                options.setProperty(
-                    RampartMessageData.KEY_RAMPART_POLICY, loadPolicy(secpol));
-            }
-
+            OMElement payload = null;
             ServiceClient serviceClient = null;
-            if (repo != null) {
+
+            if (repo != null && !"null".equals(repo)) {
                 ConfigurationContext configContext =
                     ConfigurationContextFactory.
                         createConfigurationContextFromFileSystem(repo, null);
@@ -79,16 +81,79 @@ public class StockQuoteClient {
             } else {
                 serviceClient = new ServiceClient();
             }
-            serviceClient.engageModule("addressing");
-            if (secpol != null && secpol.length() > 0) {
-                serviceClient.engageModule("rampart");
+
+            if ("customquote".equals(mode)) {
+                payload = StockQuoteHandler.createCustomQuoteRequest(symbol);
+                options.setAction("urn:getQuote");
+            } else if ("fullquote".equals(mode)) {
+                payload = StockQuoteHandler.createFullQuoteRequest(symbol);
+                options.setAction("urn:getFullQuote");
+            } else if ("placeorder".equals(mode)) {
+                price = getRandom(100, 0.9, true);
+                quantity = (int) getRandom(10000, 1.0, true);
+                payload = StockQuoteHandler.createPlaceOrderRequest(price, quantity, symbol);
+                options.setAction("urn:placeOrder");
+            } else if ("marketactivity".equals(mode)) {
+                payload = StockQuoteHandler.createMarketActivityRequest();
+                options.setAction("urn:getMarketActivity");
+            } else if ("quote".equals(mode)) {
+                payload = StockQuoteHandler.createStandardQuoteRequest(symbol);
+                options.setAction("urn:getQuote");
             }
 
+            // set addressing, transport and proxy url
+            if (addUrl != null && !"null".equals(addUrl)) {
+                serviceClient.engageModule("addressing");
+                options.setTo(new EndpointReference(addUrl));
+            }
+            if (trpUrl != null && !"null".equals(trpUrl)) {
+                options.setProperty(Constants.Configuration.TRANSPORT_URL, trpUrl);
+            }
+            if (prxUrl != null && !"null".equals(prxUrl)) {
+                HttpTransportProperties.ProxyProperties proxyProperties =
+                    new HttpTransportProperties.ProxyProperties();
+                URL url = new URL(prxUrl);
+                proxyProperties.setProxyName(url.getHost());
+                proxyProperties.setProxyPort(url.getPort());
+                proxyProperties.setUserName("");
+                proxyProperties.setPassWord("");
+                proxyProperties.setDomain("");
+                options.setProperty(HTTPConstants.PROXY, proxyProperties);
+            }
+
+            // apply any service policies if any
+            if (svcPolicy != null && !"null".equals(svcPolicy) && svcPolicy.length() > 0) {
+                serviceClient.engageModule("addressing");
+                serviceClient.engageModule("rampart");
+                options.setProperty(
+                    RampartMessageData.KEY_RAMPART_POLICY, loadPolicy(svcPolicy));
+            }
+
+            
             serviceClient.setOptions(options);
 
-            OMElement result = serviceClient.sendReceive(getQuote).getFirstElement();
-            System.out.println("Standard :: Stock price = $" +
-                StockQuoteHandler.parseStandardResponsePayload(result));
+            if ("placeorder".equals(mode)) {
+                serviceClient.fireAndForget(payload);
+                Thread.sleep(5000);
+                System.out.println("Order placed for " + quantity + " shares of stock " +
+                symbol + " at a price of $ " + price);
+
+            } else {
+                OMElement result = serviceClient.sendReceive(payload);
+                if("customquote".equals(mode)) {
+                    System.out.println("Custom :: Stock price = $" +
+                    StockQuoteHandler.parseCustomQuoteResponse(result));
+                } else if ("quote".equals(mode)) {
+                    System.out.println("Standard :: Stock price = $" +
+                        StockQuoteHandler.parseStandardQuoteResponse(result));
+                } else if ("fullquote".equals(mode)) {
+                    System.out.println("Full :: Average price = $" +
+                        StockQuoteHandler.parseFullQuoteResponse(result));
+                } else if ("marketactivity".equals(mode)) {
+                    System.out.println("Activity :: Average price = $" +
+                        StockQuoteHandler.parseMarketActivityResponse(result));   
+                }
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -98,6 +163,12 @@ public class StockQuoteClient {
     private static Policy loadPolicy(String xmlPath) throws Exception {
         StAXOMBuilder builder = new StAXOMBuilder(xmlPath);
         return PolicyEngine.getPolicy(builder.getDocumentElement());
+    }
+
+    private static double getRandom(double base, double varience, boolean onlypositive) {
+        double rand = Math.random();
+        return (base + ((rand > 0.5 ? 1 : -1) * varience * base * rand))
+            * (onlypositive ? 1 : (rand > 0.5 ? 1 : -1));
     }
 
 }

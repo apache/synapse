6c42de0379438939ed43c6de227d4bab54d03e05
Chamikara Madhusanka Jayalath <chamikara@apache.org>
2007-04-23 10:06:52 +0000

    More changes for Sandesha2 86
    git-svn-id: https://svn.apache.org/repos/asf/webservices/sandesha/trunk/java@531408 13f79535-47bb-0310-9956-ffa450edef68

--- samples/conf/userguide/SyncEchoClient.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export SANDESHA2_CLASS_PATH=./UserguideSampleClients.jar
-for f in ./lib/*.jar
-do
-  SANDESHA2_CLASS_PATH=$SANDESHA2_CLASS_PATH:$f
-done
-
-java -cp $SANDESHA2_CLASS_PATH sandesha2.samples.userguide.SyncEchoClient ./

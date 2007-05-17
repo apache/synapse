616b9f3d2a13ce27982155e3a78a8f3032aa6739
Chathura Ekanayake <chathura_ce@apache.org>
2007-05-17 09:36:09 +0000

    Minor fix to the class mediator sample class. Added license to the sample 600.
    git-svn-id: https://svn.eu.apache.org/repos/asf/webservices/synapse/trunk@538848 13f79535-47bb-0310-9956-ffa450edef68

--- java/modules/samples/src/main/java/samples/mediators/DiscountQuoteMediator.java
+++ java/modules/samples/src/main/java/samples/mediators/DiscountQuoteMediator.java
@@ -1,5 +1,23 @@
-package samples.mediators;
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *   * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 
+package samples.mediators;
 
 import org.apache.synapse.MessageContext;
 import org.apache.synapse.Mediator;
@@ -40,6 +58,7 @@ public class DiscountQuoteMediator implements Mediator {
         //Special discount of 5% offers for the first responses as set in the bonusFor property
         if (bonusCount <= bonusNo) {
             lastPrice = new Double(lastPrice.doubleValue() - lastPrice.doubleValue() * 0.05);
+            bonusCount++;
         }
 
         String discountedPrice = lastPrice.toString();
@@ -81,6 +100,5 @@ public class DiscountQuoteMediator implements Mediator {
     public String getBonusFor(){
         return bonusFor;
     }
-
 }
 

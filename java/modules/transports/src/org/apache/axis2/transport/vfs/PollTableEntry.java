f391d4ef5ca62db6eb983b2aabe033a612ccf6d8
Asankha Chamath Perera <asankha@apache.org>
2007-07-31 18:20:25 +0000

    checkin new JMS and file transport code - part 2
    git-svn-id: https://svn.eu.apache.org/repos/asf/webservices/synapse/trunk@561421 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/modules/transports/src/org/apache/axis2/transport/vfs/PollTableEntry.java
@@ -0,0 +1,175 @@
+/*
+*  Licensed to the Apache Software Foundation (ASF) under one
+*  or more contributor license agreements.  See the NOTICE file
+*  distributed with this work for additional information
+*  regarding copyright ownership.  The ASF licenses this file
+*  to you under the Apache License, Version 2.0 (the
+*  "License"); you may not use this file except in compliance
+*  with the License.  You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*   * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*/
+package org.apache.axis2.transport.vfs;
+
+public class PollTableEntry {
+
+    // status of last scan
+    public static final int SUCCSESSFUL = 0;
+    public static final int WITH_ERRORS = 1;
+    public static final int FAILED      = 2;
+
+    // operation after scan
+    public static final int DELETE = 0;
+    public static final int MOVE   = 1;
+
+    /** Axis2 service name */
+    private String serviceName;
+    /** File or Directory to scan */
+    private String fileURI;
+    /** file name pattern for a directory or compressed file entry */
+    private String fileNamePattern;
+    /** Content-Type to use for the message */
+    private String contentType;
+
+    /** last poll performed at */
+    private long lastPollTime;
+    /** duration in ms between successive polls */
+    private long pollInterval;
+    /** next poll time */
+    private long nextPollTime;
+    /** state of the last poll */
+    private int lastPollState;
+    /** action to take after a successful poll */
+    private int actionAfterProcess = DELETE;
+    /** action to take after a poll with errors */
+    private int actionAfterErrors = DELETE;
+    /** action to take after a failed poll */
+    private int actionAfterFailure = DELETE;
+
+    /** where to move the file after processing */
+    private String moveAfterProcess;
+    /** where to move the file after encountering some errors */
+    private String moveAfterErrors;
+    /** where to move the file after total failure */
+    private String moveAfterFailure;
+
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getFileURI() {
+        return fileURI;
+    }
+
+    public void setFileURI(String fileURI) {
+        this.fileURI = fileURI;
+    }
+
+    public String getFileNamePattern() {
+        return fileNamePattern;
+    }
+
+    public void setFileNamePattern(String fileNamePattern) {
+        this.fileNamePattern = fileNamePattern;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public long getLastPollTime() {
+        return lastPollTime;
+    }
+
+    public void setLastPollTime(long lastPollTime) {
+        this.lastPollTime = lastPollTime;
+    }
+
+    public long getPollInterval() {
+        return pollInterval;
+    }
+
+    public void setPollInterval(long pollInterval) {
+        this.pollInterval = pollInterval;
+    }
+
+    public long getNextPollTime() {
+        return nextPollTime;
+    }
+
+    public void setNextPollTime(long nextPollTime) {
+        this.nextPollTime = nextPollTime;
+    }
+
+    public int getLastPollState() {
+        return lastPollState;
+    }
+
+    public void setLastPollState(int lastPollState) {
+        this.lastPollState = lastPollState;
+    }
+
+    public int getActionAfterProcess() {
+        return actionAfterProcess;
+    }
+
+    public void setActionAfterProcess(int actionAfterProcess) {
+        this.actionAfterProcess = actionAfterProcess;
+    }
+
+    public int getActionAfterErrors() {
+        return actionAfterErrors;
+    }
+
+    public void setActionAfterErrors(int actionAfterErrors) {
+        this.actionAfterErrors = actionAfterErrors;
+    }
+
+    public int getActionAfterFailure() {
+        return actionAfterFailure;
+    }
+
+    public void setActionAfterFailure(int actionAfterFailure) {
+        this.actionAfterFailure = actionAfterFailure;
+    }
+
+    public String getMoveAfterProcess() {
+        return moveAfterProcess;
+    }
+
+    public void setMoveAfterProcess(String moveAfterProcess) {
+        this.moveAfterProcess = moveAfterProcess;
+    }
+
+    public String getMoveAfterErrors() {
+        return moveAfterErrors;
+    }
+
+    public void setMoveAfterErrors(String moveAfterErrors) {
+        this.moveAfterErrors = moveAfterErrors;
+    }
+
+    public String getMoveAfterFailure() {
+        return moveAfterFailure;
+    }
+
+    public void setMoveAfterFailure(String moveAfterFailure) {
+        this.moveAfterFailure = moveAfterFailure;
+    }
+}

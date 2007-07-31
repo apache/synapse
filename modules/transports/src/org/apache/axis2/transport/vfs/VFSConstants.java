2cadc6abf91c40a230020e78f0516cd0dd0417ba
Asankha Chamath Perera <asankha@apache.org>
2007-07-31 18:20:25 +0000

    checkin new JMS and file transport code - part 2
    git-svn-id: https://svn.eu.apache.org/repos/asf/webservices/synapse/trunk/java@561421 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/transports/src/org/apache/axis2/transport/vfs/VFSConstants.java
@@ -0,0 +1,34 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *   * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.apache.axis2.transport.vfs;
+
+public class VFSConstants {
+    public static final String TRANSPORT_FILE_ACTION_AFTER_PROCESS = "transport.vfs.ActionAfterProcess";
+    public static final String TRANSPORT_FILE_ACTION_AFTER_ERRORS = "transport.vfs.ActionAfterErrors";
+    public static final String TRANSPORT_FILE_ACTION_AFTER_FAILURE = "transport.vfs.ActionAfterFailure";
+    public static final String TRANSPORT_FILE_FILE_URI = "transport.vfs.FileURI";
+    public static final String TRANSPORT_FILE_FILE_NAME_PATTERN = "transport.vfs.FileNamePattern";
+    public static final String TRANSPORT_FILE_CONTENT_TYPE = "transport.vfs.ContentType";
+
+    public static final String REPLY_FILE_URI = "transport.vfs.ReplyFileURI";
+    public static final String REPLY_FILE_NAME = "transport.vfs.ReplyFileName";
+
+    public static final String DEFAULT_RESPONSE_FILE = "response.xml";
+}

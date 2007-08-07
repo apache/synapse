935b00763513a19a26a3de1c84148c9447917ffc
Paul Fremantle <pzf@apache.org>
2007-08-07 16:09:05 +0000

    added SimpleQuartz startup, factory, synapse Job Interface and a simple MessageInjector job
    git-svn-id: https://svn.eu.apache.org/repos/asf/webservices/synapse/trunk@563541 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/modules/core/src/main/java/org/apache/synapse/startup/quartz/SimpleQuartzSerializer.java
@@ -0,0 +1,44 @@
+package org.apache.synapse.startup.quartz;
+
+import java.util.Iterator;
+
+import javax.xml.namespace.QName;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axiom.om.OMNamespace;
+import org.apache.synapse.config.xml.Constants;
+import org.apache.synapse.config.xml.StartupSerializer;
+import org.apache.synapse.Startup;
+import org.apache.synapse.SynapseException;
+
+public class SimpleQuartzSerializer implements StartupSerializer {
+	
+	public void serializeStartup(OMElement parent, Startup s) {
+		if (!(s instanceof SimpleQuartz)) throw new SynapseException("called SimpleQuartzSerializer on some other kind of startup"+s.getClass().getName());
+		SimpleQuartz sq = (SimpleQuartz)s;
+		OMFactory fac = parent.getOMFactory();
+		OMNamespace nullNS = fac.createOMNamespace("", "");
+		OMElement job = fac.createOMElement(SimpleQuartzFactory.JOB,  parent);
+		job.addAttribute("class", sq.getJobClass(), nullNS);
+		if (sq.isSimple()) {
+			OMElement el = fac.createOMElement(new QName(Constants.SYNAPSE_NAMESPACE,"simpletrigger"), job);
+			if (sq.getCount()==-1) {
+				el.addAttribute("forever","true",nullNS);
+			} else {
+				el.addAttribute("count",Integer.toString(sq.getCount()),nullNS);
+			}
+			el.addAttribute("interval", Long.toString(sq.getInterval()), nullNS);
+		} else {
+			OMElement el = fac.createOMElement(new QName(Constants.SYNAPSE_NAMESPACE,"crontrigger"), job);
+			el.addAttribute("expression", sq.getCron(), nullNS);
+		}
+		Iterator it = sq.getProperties().iterator();
+		while (it.hasNext()) {
+			OMElement prop = (OMElement)it.next();
+			job.addChild(prop.cloneOMElement());
+		}
+		
+	}
+
+}

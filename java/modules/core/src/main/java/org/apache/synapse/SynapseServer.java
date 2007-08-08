2fa4552c019d6fa6be8062d8fa667b56e217d287
Indika Kumara <indika@apache.org>
2007-08-08 04:39:51 +0000

    Enable synapse run as a web application done required changes to configuration + some classes + scripts
    git-svn-id: https://svn.eu.apache.org/repos/asf/webservices/synapse/trunk@563740 13f79535-47bb-0310-9956-ffa450edef68

--- java/modules/core/src/main/java/org/apache/synapse/SynapseServer.java
+++ java/modules/core/src/main/java/org/apache/synapse/SynapseServer.java
@@ -19,19 +19,10 @@
 
 package org.apache.synapse;
 
-import org.apache.axis2.util.OptionsParser;
-import org.apache.axis2.context.ConfigurationContext;
-import org.apache.axis2.context.ConfigurationContextFactory;
-import org.apache.axis2.engine.ListenerManager;
-import org.apache.axis2.description.TransportInDescription;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import javax.xml.namespace.QName;
 import java.io.File;
-import java.util.Iterator;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 /**
  * Starts all transports as specified on the axis2.xml
@@ -52,90 +43,8 @@ public class SynapseServer {
         if (args.length != 1 || !new File(args[0]).exists()) {
             printUsage();
         }
-
-        System.out.println("[SynapseServer] Using the Axis2 Repository "
-                + new File(args[0]).getAbsolutePath());
-
-        try {
-            ConfigurationContext configctx = ConfigurationContextFactory.
-                createConfigurationContextFromFileSystem(args[0], null);
-
-            ListenerManager listenerManager = configctx.getListenerManager();
-            if (listenerManager == null) {
-                listenerManager = new ListenerManager();
-                listenerManager.init(configctx);
-            }
-
-            // decide on HTTP port to execute
-            selectPort(configctx);
-
-            Iterator iter = configctx.getAxisConfiguration().
-                getTransportsIn().keySet().iterator();
-            while (iter.hasNext()) {
-                String trp = (String) iter.next();
-                TransportInDescription trsIn = (TransportInDescription)
-                    configctx.getAxisConfiguration().getTransportsIn().get(trp);
-                listenerManager.addListener(trsIn, false);
-                String msg = "[SynapseServer] Starting transport " + trsIn.getName();
-                if (trsIn.getParameter("port") != null) {
-                    msg += " on port " + trsIn.getParameter("port").getValue();
-                }
-                System.out.println(msg);
-            }
-            System.out.println("[SynapseServer] Ready");
-
-        } catch (Throwable t) {
-            t.printStackTrace();
-            System.out.println("[SynapseServer] Startup failed...");
-        }
+        ServerManager.axis2Repolocation = args[0];
+        ServerManager serverManager = ServerManager.getInstance();
+        serverManager.start();
     }
-
-    private static void selectPort(ConfigurationContext configCtx) {
-        // check if configured port is available
-        TransportInDescription trsIn = (TransportInDescription)
-            configCtx.getAxisConfiguration().getTransportsIn().get("http");
-
-        if (trsIn != null) {
-
-            int port = 8080;
-
-            String strPort = System.getProperty("port");
-            if(strPort != null) {
-                // port is specified as a VM parameter
-                try {
-                    port = new Integer(strPort).intValue();
-                } catch (NumberFormatException e) {
-                    // user supplied parameter is not a valid integer. so use the port in configuration.
-                    log.error("Given port is not a valid integer. Port specified in the configuration is used for the server.");
-                    port = Integer.parseInt(trsIn.getParameter("port").getValue().toString());
-                }
-
-            } else {
-                port = Integer.parseInt(trsIn.getParameter("port").getValue().toString());
-            }
-
-            while (true) {
-                ServerSocket sock = null;
-                try {
-                    sock = new ServerSocket(port);
-                    trsIn.getParameter("port").setValue(Integer.toString(port));
-                    break;
-                } catch (Exception e) {
-                	System.out.println("[SynapseServer] Port "+port+" already in use. Trying alternate");
-                    if (port == 8080) {
-                        port = 8008;
-                    } else {
-                        port++;
-                    }
-                } finally {
-                    if (sock != null) {
-                        try {
-                            sock.close();
-                        } catch (Exception e) {}
-                    }
-                }
-            }
-        }
-    }
-
 }

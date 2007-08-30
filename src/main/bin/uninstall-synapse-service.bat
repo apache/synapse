91c13f88ec23410673b1f865c4257fc0733aed4f
Indika Kumara <indika@apache.org>
2007-08-30 10:45:52 +0000

    fixed a bug which has occured when use synapse-daemon script in solaris OS some minor improvements
    git-svn-id: https://svn.apache.org/repos/asf/webservices/synapse/trunk/java@571110 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/bin/uninstall-synapse-service.bat
+++ src/main/bin/uninstall-synapse-service.bat
@@ -19,16 +19,17 @@ set _REALPATH=%~dp0..\
 
 rem Decide on the wrapper binary.
 set _WRAPPER_BASE=wrapper
-set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-x86-32.exe
+set _WRAPPER_DIR=%_REALPATH%bin\native\
+set _WRAPPER_EXE=%_WRAPPER_DIR%%_WRAPPER_BASE%-windows-x86-32.exe
 if exist "%_WRAPPER_EXE%" goto conf
-set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-x86-64.exe
+set _WRAPPER_EXE=%_WRAPPER_DIR%%_WRAPPER_BASE%-windows-x86-64.exe
 if exist "%_WRAPPER_EXE%" goto conf
-set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%.exe
+set _WRAPPER_EXE=%_WRAPPER_DIR%%_WRAPPER_BASE%.exe
 if exist "%_WRAPPER_EXE%" goto conf
 echo Unable to locate a Wrapper executable using any of the following names:
-echo %_REALPATH%%_WRAPPER_BASE%-windows-x86-32.exe
-echo %_REALPATH%%_WRAPPER_BASE%-windows-x86-64.exe
-echo %_REALPATH%%_WRAPPER_BASE%.exe
+echo %_WRAPPER_DIR%%_WRAPPER_BASE%-windows-x86-32.exe
+echo %_WRAPPER_DIR%%_WRAPPER_BASE%-windows-x86-64.exe
+echo %_WRAPPER_DIR%%_WRAPPER_BASE%.exe
 pause
 goto :eof
 

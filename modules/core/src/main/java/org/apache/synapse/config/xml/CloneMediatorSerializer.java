c2a5219aa0acafe2100b1cc104d654102d9c2f7d
Endera Linton <ruwan@apache.org>
2007-09-18 04:45:55 +0000

    Cleaning up the clone mediator and EIP helper classes
    git-svn-id: https://svn.apache.org/repos/asf/webservices/synapse/trunk/java@576693 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core/src/main/java/org/apache/synapse/config/xml/CloneMediatorSerializer.java
+++ modules/core/src/main/java/org/apache/synapse/config/xml/CloneMediatorSerializer.java
@@ -1,3 +1,22 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *   * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
 package org.apache.synapse.config.xml;
 
 import org.apache.axiom.om.OMElement;
@@ -8,10 +27,28 @@ import org.apache.synapse.mediators.eip.Target;
 import java.util.Iterator;
 
 /**
- * 
+ * This will serialize the CloneMediator to the xml configuration as specified bellow
+ *
+ * <pre>
+ *  &lt;clone continueParent=(true | false)&gt;
+ *   &lt;target to="TO address" [soapAction="urn:Action"] sequence="sequence ref"
+ *                                                         endpoint="endpoint ref"&gt;
+ *    &lt;sequence&gt; (mediator +) &lt;/sequence&gt;
+ *    &lt;endpoint&gt; endpoint &lt;/endpoint&gt;
+ *   &lt;/target&gt;
+ *  &lt;/iterate&gt;
+ * </pre>
  */
 public class CloneMediatorSerializer extends AbstractMediatorSerializer {
 
+    /**
+     * This method will implement the serializeMediator method of the MediatorSerializer interface
+     *
+     * @param parent - OMElement describing the parent element to which the newlly generated
+     *  clone element should be attached as a child, if provided
+     * @param m - Mediator of the type CloneMediator which is subjected to the serialization
+     * @return OMElement serialized in to xml from the given parameters
+     */
     public OMElement serializeMediator(OMElement parent, Mediator m) {
 
         OMElement cloneElem = fac.createOMElement("clone", synNS);
@@ -29,6 +66,7 @@ public class CloneMediatorSerializer extends AbstractMediatorSerializer {
             }
         }
 
+        // attach the serialized element to the parent if specified
         if (parent != null) {
             parent.addChild(cloneElem);
         }
@@ -36,6 +74,13 @@ public class CloneMediatorSerializer extends AbstractMediatorSerializer {
         return cloneElem;
     }
 
+    /**
+     * This method will implement the getMediatorClassName method of the
+     * MediatorSerializer interface
+     * 
+     * @return String representing the full class name of the Mediator
+     *  which is serialized by this Serializer
+     */
     public String getMediatorClassName() {
         return CloneMediator.class.getName();
     }

483fbacc7c7462c36c90b1b3564bfe180be5a937
Endera Linton <ruwan@apache.org>
2007-10-06 06:42:46 +0000

    Fixing the issue with the sendBack in Axis2SynapseEnvironment.java
    git-svn-id: https://svn.eu.apache.org/repos/asf/webservices/synapse/trunk/java@582461 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core/src/main/java/org/apache/synapse/core/axis2/Axis2SynapseEnvironment.java
+++ modules/core/src/main/java/org/apache/synapse/core/axis2/Axis2SynapseEnvironment.java
@@ -24,20 +24,20 @@ import org.apache.axis2.context.ConfigurationContext;
 import org.apache.axis2.context.ServiceContext;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.synapse.SynapseConstants;
 import org.apache.synapse.Mediator;
 import org.apache.synapse.MessageContext;
+import org.apache.synapse.SynapseConstants;
 import org.apache.synapse.SynapseException;
-import org.apache.synapse.util.UUIDGenerator;
 import org.apache.synapse.config.SynapseConfiguration;
-import org.apache.synapse.endpoints.utils.EndpointDefinition;
 import org.apache.synapse.core.SynapseEnvironment;
+import org.apache.synapse.endpoints.utils.EndpointDefinition;
+import org.apache.synapse.mediators.base.SequenceMediator;
 import org.apache.synapse.statistics.StatisticsCollector;
 import org.apache.synapse.statistics.StatisticsUtils;
-
+import org.apache.synapse.util.UUIDGenerator;
 
 /**
- * <p> This is the Axis2 implementation of the MessageContext
+ * This is the Axis2 implementation of the SynapseEnvironment
  */
 public class Axis2SynapseEnvironment implements SynapseEnvironment {
 
@@ -70,7 +70,7 @@ public class Axis2SynapseEnvironment implements SynapseEnvironment {
             StatisticsUtils.processSequenceStatistics(synCtx);
         }
 
-        // if this is a response to a proxy service 
+        // if this is a response to a proxy service
         if (synCtx.getProperty(SynapseConstants.PROXY_SERVICE) != null) {
 
             if (synCtx.getConfiguration().getProxyService((String) synCtx.getProperty(
@@ -120,19 +120,68 @@ public class Axis2SynapseEnvironment implements SynapseEnvironment {
         return true;
     }
 
-    public void send(EndpointDefinition endpoint, MessageContext synCtx) {
-        if (synCtx.isResponse())
-            Axis2Sender.sendBack(synCtx);
-        else
-            Axis2Sender.sendOn(endpoint, synCtx);
+    public void injectAsync(final MessageContext synCtx, SequenceMediator seq) {
+        if (log.isDebugEnabled()) {
+            log.debug("Injecting MessageContext for asynchronous mediation using the "
+                + seq.getName() == null? "Anonymous" : seq.getName());
+        }
+        synCtx.setEnvironment(this);
+        // todo: do we need to have this in here ? ruwan
+        if (synCtx.isResponse()) {
+            //Process statistics related to a sequence which has send mediator as a child,end point
+            StatisticsUtils.processEndPointStatistics(synCtx);
+            StatisticsUtils.processProxyServiceStatistics(synCtx);
+            StatisticsUtils.processSequenceStatistics(synCtx);
+        }
+
+        ((Axis2MessageContext) synCtx).getAxis2MessageContext()
+            .getConfigurationContext().getThreadPool().execute(new SynapseWorker(seq, synCtx));
+
     }
 
+    /**
+     * This will be used for sending the message provided, to the endpoint specified by the
+     * EndpointDefinition using the axis2 environment.
+     *
+     * @param endpoint - EndpointDefinition to be used to find the endpoint information
+     *                      and the properties of the sending process
+     * @param synCtx   - Synapse MessageContext to be sent
+     */
+    public void send(EndpointDefinition endpoint, MessageContext synCtx) {
+        if (synCtx.isResponse()) {
+
+            if (endpoint != null) {
+                // not sure whether we need to collect statistics here
+                StatisticsUtils.processEndPointStatistics(synCtx);
+                StatisticsUtils.processProxyServiceStatistics(synCtx);
+                StatisticsUtils.processAllSequenceStatistics(synCtx);
+
+                Axis2Sender.sendOn(endpoint, synCtx);
+
+            } else {
+                Axis2Sender.sendBack(synCtx);
+            }
+        } else {
+            Axis2Sender.sendOn(endpoint, synCtx);
+        }
+    }
+
+    /**
+     * This method will be used to create a new MessageContext in the Axis2 environment for
+     * synapse. This will set all the relevant parts to the messagecontext, but for this message
+     * context to be usefull creator has to fill in the data like envelope and operation context
+     * and so on. This will set a default envelope of type soap12 and a new messageID for the
+     * created message along with the ConfigurationContext is being set in to the message
+     * correctly.
+     *
+     * @return Synapse MessageContext with the underlying axis2 message context set
+     */
     public MessageContext createMessageContext() {
 
         if (log.isDebugEnabled()) {
             log.debug("Creating Message Context");
         }
-        
+
         org.apache.axis2.context.MessageContext axis2MC
                 = new org.apache.axis2.context.MessageContext();
         axis2MC.setConfigurationContext(this.configContext);
@@ -162,11 +211,53 @@ public class Axis2SynapseEnvironment implements SynapseEnvironment {
     /**
      * To set the StatisticsCollector
      *
-     * @param collector
+     * @param collector - Statistics collector to be set
      */
     public void setStatisticsCollector(StatisticsCollector collector) {
         this.statisticsCollector = collector;
     }
 
+    /**
+     * This inner class will be used as the executer for the injectAsync method for the
+     * sequence mediation
+     */
+    private class SynapseWorker implements Runnable {
+
+        /** Mediator to be executed */
+        private Mediator seq = null;
+
+        /** MessageContext to be mediated using the mediator */
+        private MessageContext synCtx = null;
+
+        /**
+         * Constructor of the SynapseWorker which sets the sequence and the message context
+         *
+         * @param seq    - Sequence Mediator to be set
+         * @param synCtx - Synapse MessageContext to be set
+         */
+        public SynapseWorker(Mediator seq, MessageContext synCtx) {
+            this.seq = seq;
+            this.synCtx = synCtx;
+        }
+
+        /**
+         * Constructor od the SynapseWorker which sets the provided message context and the
+         * main sequence as the sequence for mediation
+         *
+         * @param synCtx - Synapse MessageContext to be set
+         */
+        public SynapseWorker(MessageContext synCtx) {
+            this.synCtx = synCtx;
+            seq = synCtx.getMainSequence();
+        }
+
+        /**
+         * Execution method of the thread. This will just call the mediation of the specified
+         * Synapse MessageContext using the specified Sequence Mediator
+         */
+        public void run() {
+            seq.mediate(synCtx);
+        }
+    }
 
 }

214970c12e923c1f6fe97bbc867c8e91cd3427bb
Upul Godage <upul@apache.org>
2007-12-19 04:29:32 +0000

    SYNAPSE-203 made final MetricsConstants values
    git-svn-id: https://svn.eu.apache.org/repos/asf/webservices/synapse/trunk@605419 13f79535-47bb-0310-9956-ffa450edef68

--- java/modules/core/src/main/java/org/apache/synapse/metrics/MetricsConstants.java
+++ java/modules/core/src/main/java/org/apache/synapse/metrics/MetricsConstants.java
@@ -18,11 +18,12 @@
  */
 
 package org.apache.synapse.metrics;
-/*
+
+/**
  * 
  */
-
 public final class MetricsConstants {
-    public static String GLOBAL_REQUEST_COUNTER = "__GLOBAL_REQUEST_COUNTER__";
-    public static String REQUEST_RECEIVED_TIME = "__REQUEST_RECEIVED_TIME__";
+    public static final String GLOBAL_REQUEST_COUNTER = "__GLOBAL_REQUEST_COUNTER__";
+    public static final String REQUEST_RECEIVED_TIME = "__REQUEST_RECEIVED_TIME__";
 }
+

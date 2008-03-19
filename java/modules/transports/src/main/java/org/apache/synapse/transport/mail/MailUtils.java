7fd98b9857c3a19b6709fc7043cc46ebb1c2fce0
Endera Linton <ruwan@apache.org>
2008-03-19 19:33:50 +0000

    Fixing issue SYNAPSE-256, (it seems gmail send the contentType as text/plain rather than plain/text)
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk@638978 13f79535-47bb-0310-9956-ffa450edef68

--- java/modules/transports/src/main/java/org/apache/synapse/transport/mail/MailUtils.java
+++ java/modules/transports/src/main/java/org/apache/synapse/transport/mail/MailUtils.java
@@ -79,12 +79,15 @@ public class MailUtils extends BaseUtils {
                              contType.indexOf(SOAP12Constants.SOAP_12_CONTENT_TYPE) != -1)) {
                             // this part is a SOAP 11 or 12 payload, treat this as the message
                             return mbp.getInputStream();
-                        } else if (mbp == null && contType.indexOf("plain/text") != -1) {
+                        } else if (mbp != null && (contType.indexOf("plain/text") != -1
+                                || contType.indexOf("text/plain") != -1)) {
                             firstTextPart = mbp;
                         }
                     }
                     // if a soap 11 or soap12 payload was not found, treat first text part as message
-                    return firstTextPart.getInputStream();
+                    if (firstTextPart != null) {
+                        return firstTextPart.getInputStream();
+                    }
 
                 } else {
                     return ((Message) message).getInputStream();
@@ -94,6 +97,7 @@ public class MailUtils extends BaseUtils {
             handleException("Error creating an input stream to : " +
                 ((Message) message).getMessageNumber(), e);
         }
+        
         return null;
     }
 

8246fa884d81587cd63207a9b008ebc7741cd092
Indika Kumara <indika@apache.org>
2008-03-25 11:28:52 +0000

    Initial fix for SYNAPE-223
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk@640773 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/modules/core/src/main/java/org/apache/synapse/util/RMIRegistryController.java
@@ -0,0 +1,73 @@
+/**
+ *
+ */
+package org.apache.synapse.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.synapse.SynapseException;
+
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.NoSuchObjectException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class RMIRegistryController {
+
+    public static final Log log = LogFactory.getLog(RMIRegistryController.class);
+
+    private static RMIRegistryController ourInstance = new RMIRegistryController();
+
+    private Registry localRegistry;
+
+    public static RMIRegistryController getInstance() {
+        return ourInstance;
+    }
+
+    private RMIRegistryController() {
+    }
+
+    /**
+     * Creates a RMI local registry with given port
+     *
+     * @param port The port
+     */
+    public void createLocalRegistry(int port) {
+        try {
+            localRegistry = LocateRegistry.createRegistry(port);
+        } catch (RemoteException e) {
+            String msg = "Couldn't create a local registry(RMI) : port " + port +
+                    " already in use.";
+            handleException(msg, e);
+        }
+    }
+
+    /**
+     * removes if there is a RMI local registry instance
+     */
+    public void removeLocalRegistry() {
+        if (localRegistry != null) {
+            try {
+                UnicastRemoteObject.unexportObject(localRegistry, true);
+            } catch (NoSuchObjectException e) {
+                String msg = "Error when stoping localregistry(RMI)";
+                handleException(msg, e);
+            }
+        }
+    }
+
+
+    /**
+     * Helper methods for handle errors.
+     *
+     * @param msg The error message
+     * @param e   The exception
+     */
+    private static void handleException(String msg, Exception e) {
+
+        log.error(msg, e);
+        throw new SynapseException(msg, e);
+    }
+
+}

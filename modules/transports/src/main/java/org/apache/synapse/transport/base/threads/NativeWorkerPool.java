05417af1506220e9153c7a8f18c8cfc60edd7b42
Asankha Chamath Perera <asankha@apache.org>
2008-04-08 10:01:47 +0000

    fix incorrect count of bytes sent/received remove duplicate copy of BackportWorkerPool and NativeWorkerPool classes from nhttp package
    git-svn-id: https://svn.eu.apache.org/repos/asf/synapse/trunk/java@645821 13f79535-47bb-0310-9956-ffa450edef68

--- modules/transports/src/main/java/org/apache/synapse/transport/base/threads/NativeWorkerPool.java
+++ modules/transports/src/main/java/org/apache/synapse/transport/base/threads/NativeWorkerPool.java
@@ -1,3 +1,22 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *   * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
 package org.apache.synapse.transport.base.threads;
 
 import org.apache.commons.logging.Log;
@@ -29,25 +48,29 @@ public class NativeWorkerPool implements WorkerPool {
             queueLength == -1 ?
                 new LinkedBlockingQueue() :
                 new LinkedBlockingQueue(queueLength),
-            new NativeWorkerPool.BackportThreadFactory(new ThreadGroup(threadGroupName), threadGroupId));
+            new NativeThreadFactory(new ThreadGroup(threadGroupName), threadGroupId));
     }
 
     public void execute(Runnable task) {
         executor.execute(task);
     }
 
+    public int getActiveCount() {
+        return ((ThreadPoolExecutor) executor).getActiveCount();
+    }
+
     /**
      * This is a simple ThreadFactory implementation using java.util.concurrent
      * Creates threads with the given name prefix
      */
-    public class BackportThreadFactory implements
+    public class NativeThreadFactory implements
         ThreadFactory {
 
         final ThreadGroup group;
         final AtomicInteger count;
         final String namePrefix;
 
-        public BackportThreadFactory(final ThreadGroup group, final String namePrefix) {
+        public NativeThreadFactory(final ThreadGroup group, final String namePrefix) {
             super();
             this.count = new AtomicInteger(1);
             this.group = group;

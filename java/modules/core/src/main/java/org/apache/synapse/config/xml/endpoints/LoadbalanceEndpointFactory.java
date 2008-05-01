7714ef44ec8dd92d9e6d7ff5ae6ab0121996c728
Andreas Veithen <veithen@apache.org>
2008-05-01 22:05:49 +0000

    Corrected misspelled method name.
    git-svn-id: https://svn.eu.apache.org/repos/asf/synapse/trunk@652686 13f79535-47bb-0310-9956-ffa450edef68

--- java/modules/core/src/main/java/org/apache/synapse/config/xml/endpoints/LoadbalanceEndpointFactory.java
+++ java/modules/core/src/main/java/org/apache/synapse/config/xml/endpoints/LoadbalanceEndpointFactory.java
@@ -115,7 +115,7 @@ public class LoadbalanceEndpointFactory implements EndpointFactory {
 
             OMElement endptElem = (OMElement) iter.next();
 
-            EndpointFactory epFac = EndpointAbstractFactory.getEndpointFactroy(endptElem);
+            EndpointFactory epFac = EndpointAbstractFactory.getEndpointFactory(endptElem);
             Endpoint endpoint = epFac.createEndpoint(endptElem, true);
             endpoint.setParentEndpoint(parent);
             endpoints.add(endpoint);

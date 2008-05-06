dd61ab40e0145fc6e03fcb192f43e9c4028f18b5
Endera Linton <ruwan@apache.org>
2008-05-06 01:48:50 +0000

    Fixing the build, that I have broken :-)
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk@653664 13f79535-47bb-0310-9956-ffa450edef68

--- java/modules/core/src/main/java/org/apache/synapse/config/xml/endpoints/WSDLEndpointSerializer.java
+++ java/modules/core/src/main/java/org/apache/synapse/config/xml/endpoints/WSDLEndpointSerializer.java
@@ -38,9 +38,7 @@ import org.apache.synapse.endpoints.utils.EndpointDefinition;
  *    </wsdl>
  * </endpoint>
  */
-public class WSDLEndpointSerializer implements EndpointSerializer {
-
-    private OMFactory fac = null;
+public class WSDLEndpointSerializer extends EndpointSerializer {
 
     public OMElement serializeEndpoint(Endpoint endpoint) {
 
@@ -101,87 +99,5 @@ public class WSDLEndpointSerializer implements EndpointSerializer {
 
         return endpointElement;
     }
-
-    public void serializeQOSInformation
-            (EndpointDefinition endpointDefinition, OMElement element) {
-
-        if (SynapseConstants.FORMAT_POX.equals(endpointDefinition.getFormat())) {
-            element.addAttribute(fac.createOMAttribute("format", null, "pox"));
-        } else if (SynapseConstants.FORMAT_GET.equals(endpointDefinition.getFormat())) {
-            element.addAttribute(fac.createOMAttribute("format", null, "get"));
-        } else if (SynapseConstants.FORMAT_SOAP11.equals(endpointDefinition.getFormat())) {
-            element.addAttribute(fac.createOMAttribute("format", null, "soap11"));
-        } else if (SynapseConstants.FORMAT_SOAP12.equals(endpointDefinition.getFormat())) {
-            element.addAttribute(fac.createOMAttribute("format", null, "soap12"));
-        
-            // following two kept for backward compatibility
-        } else if (endpointDefinition.isForcePOX()) {
-            element.addAttribute(fac.createOMAttribute("format", null, "pox"));
-        } else if (endpointDefinition.isForceGET()) {
-            element.addAttribute(fac.createOMAttribute("format", null, "get"));
-        } else if (endpointDefinition.isForceSOAP11()) {
-            element.addAttribute(fac.createOMAttribute("format", null, "soap11"));
-        } else if (endpointDefinition.isForceSOAP12()) {
-            element.addAttribute(fac.createOMAttribute("format", null, "soap12"));
-        }
-        
-        int isEnableStatistics = endpointDefinition.getStatisticsState();
-        String statisticsValue = null;
-        if (isEnableStatistics == org.apache.synapse.SynapseConstants.STATISTICS_ON) {
-            statisticsValue = org.apache.synapse.config.xml.XMLConfigConstants.STATISTICS_ENABLE;
-        } else if (isEnableStatistics == org.apache.synapse.SynapseConstants.STATISTICS_OFF) {
-            statisticsValue = org.apache.synapse.config.xml.XMLConfigConstants.STATISTICS_DISABLE;
-        }
-        if (statisticsValue != null) {
-            element.addAttribute(fac.createOMAttribute(
-                    org.apache.synapse.config.xml.XMLConfigConstants.STATISTICS_ATTRIB_NAME, null, statisticsValue));
-        }
-        if (endpointDefinition.isAddressingOn()) {
-            OMElement addressing = fac.createOMElement("enableAddressing", SynapseConstants.SYNAPSE_OMNAMESPACE);
-            if (endpointDefinition.getAddressingVersion() != null) {
-                addressing.addAttribute(fac.createOMAttribute(
-                        "version", null, endpointDefinition.getAddressingVersion()));
-            }
-            if (endpointDefinition.isUseSeparateListener()) {
-                addressing.addAttribute(fac.createOMAttribute(
-                        "separateListener", null, "true"));
-            }
-            element.addChild(addressing);
-        }
-
-        if (endpointDefinition.isReliableMessagingOn()) {
-            OMElement rm = fac.createOMElement("enableRM", SynapseConstants.SYNAPSE_OMNAMESPACE);
-            if (endpointDefinition.getWsRMPolicyKey() != null) {
-                rm.addAttribute(fac.createOMAttribute(
-                        "policy", null, endpointDefinition.getWsRMPolicyKey()));
-            }
-            element.addChild(rm);
-        }
-
-        if (endpointDefinition.isSecurityOn()) {
-            OMElement sec = fac.createOMElement("enableSec", SynapseConstants.SYNAPSE_OMNAMESPACE);
-            if (endpointDefinition.getWsSecPolicyKey() != null) {
-                sec.addAttribute(fac.createOMAttribute(
-                        "policy", null, endpointDefinition.getWsSecPolicyKey()));
-            }
-            element.addChild(sec);
-        }
-
-        if (endpointDefinition.getTimeoutAction() != SynapseConstants.NONE) {
-            OMElement timeout = fac.createOMElement("timeout", SynapseConstants.SYNAPSE_OMNAMESPACE);
-            element.addChild(timeout);
-
-            OMElement duration = fac.createOMElement("duration", SynapseConstants.SYNAPSE_OMNAMESPACE);
-            duration.setText(Long.toString(endpointDefinition.getTimeoutDuration() / 1000));
-            timeout.addChild(duration);
-
-            OMElement action = fac.createOMElement("action", SynapseConstants.SYNAPSE_OMNAMESPACE);
-            if (endpointDefinition.getTimeoutAction() == SynapseConstants.DISCARD) {
-                action.setText("discard");
-            } else if (endpointDefinition.getTimeoutAction() == SynapseConstants.DISCARD_AND_FAULT) {
-                action.setText("fault");
-            }
-            timeout.addChild(action);
-        }
-    }
+    
 }

c449dae0f9a290914d1615722204e7f0c35ce155
Andreas Veithen <veithen@apache.org>
2008-06-20 18:26:48 +0000

    * Make sure that the InputSource objects returned by ResourceMap have their systemId set as Axis2 relies on this. The generated system IDs are of the form "synapse-reg:///<key>". This fixes SYNAPSE-362. * Added a unit test for WSDL/XSD import resolution with ResourceMap. This is a unit test for the improvement in SYNAPSE-200 and a regression test for issue SYNAPSE-362.
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk/java@670024 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core/src/test/java/org/apache/synapse/core/axis2/ProxyServiceTest.java
+++ modules/core/src/test/java/org/apache/synapse/core/axis2/ProxyServiceTest.java
@@ -29,6 +29,7 @@ import junit.framework.TestCase;
 
 import org.apache.axis2.description.AxisService;
 import org.apache.axis2.engine.AxisConfiguration;
+import org.apache.synapse.config.Entry;
 import org.apache.synapse.config.SynapseConfiguration;
 import org.xml.sax.InputSource;
 
@@ -49,5 +50,41 @@ public class ProxyServiceTest extends TestCase {
         // Check that the produced WSDL can be read by WSDL4J
         WSDLReader wsdlReader = WSDLFactory.newInstance().newWSDLReader();
         wsdlReader.readWSDL(null, new InputSource(new ByteArrayInputStream(baos.toByteArray())));
+    }
+    
+    /**
+     * Test a proxy with a WSDL importing another WSDL importing an XSD. The imported WSDL and
+     * XSD documents are resolved using a {@link ResourceMap} (i.e. &lt;resource> elements). The
+     * test checks that the proxy service can be built and produce a WSDL.
+     * This is a test for the feature introduced by SYNAPSE-200 and a regression test
+     * for SYNAPSE-362.
+     */
+    public void testWSDLWithPublishWSDLAndRecursiveImports() throws Exception {
+        SynapseConfiguration synCfg = new SynapseConfiguration();
+        AxisConfiguration axisCfg = new AxisConfiguration();
+        // Add local entries
+        Entry entry = new Entry();
+        entry.setType(Entry.URL_SRC);
+        entry.setSrc(getClass().getResource("root.wsdl"));
+        synCfg.addEntry("root_wsdl", entry);
+        entry = new Entry();
+        entry.setType(Entry.URL_SRC);
+        entry.setSrc(getClass().getResource("imported.xsd"));
+        synCfg.addEntry("imported_xsd", entry);
+        entry = new Entry();
+        entry.setType(Entry.URL_SRC);
+        entry.setSrc(getClass().getResource("imported.wsdl"));
+        synCfg.addEntry("imported_wsdl", entry);
+        // Build the proxy service
+        ProxyService proxyService = new ProxyService("Test");
+        proxyService.setWSDLKey("root_wsdl");
+        ResourceMap resourceMap = new ResourceMap();
+        resourceMap.addResource("imported.wsdl", "imported_wsdl");
+        resourceMap.addResource("imported.xsd", "imported_xsd");
+        proxyService.setResourceMap(resourceMap);
+        AxisService axisService = proxyService.buildAxisService(synCfg, axisCfg);
+        // Serialize the WSDL. Note that we can't parse the WSDL because it will have imports
+        // referring to locations such as "my-matches?xsd=xsd0.xsd".
+        axisService.printWSDL(new ByteArrayOutputStream());
     }
 }

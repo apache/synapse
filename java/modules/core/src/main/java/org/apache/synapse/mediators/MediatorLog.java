223027ab5b1d1dd6d760b7c5f19bb4e2b1b96e7c
Andreas Veithen <veithen@apache.org>
2008-06-25 15:05:01 +0000

    SYNAPSE-374 (Simplify the way logging is done in mediators): * added the basic infrastructure as described in the issue * applied the pattern to the SendMediator
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk@671576 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/modules/core/src/main/java/org/apache/synapse/mediators/MediatorLog.java
@@ -0,0 +1,76 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *   * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.apache.synapse.mediators;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.synapse.SynapseConstants;
+import org.apache.synapse.SynapseLog;
+
+/**
+ * Concrete implementation of the {@link SynapseLog} interface appropriate
+ * for usage in a mediator. Instances of this class should not be created
+ * directly but by using the factory method
+ * {@link AbstractMediator#getLog(org.apache.synapse.MessageContext)}.
+ * <p>
+ * Note that this is work in progress.
+ * Please refer to https://issues.apache.org/jira/browse/SYNAPSE-374 for
+ * more information.
+ */
+public class MediatorLog implements SynapseLog {
+    private static final Log traceLog = LogFactory.getLog(SynapseConstants.TRACE_LOGGER);
+    
+    private final Log defaultLog;
+    private final boolean traceOn;
+    
+    // The definition of this constructor might change...
+    public MediatorLog(Log defaultLog, boolean traceOn) {
+        this.defaultLog = defaultLog;
+        this.traceOn = traceOn;
+    }
+
+    public boolean isTraceOrDebugEnabled() {
+        return traceOn || defaultLog.isDebugEnabled();
+    }
+
+    /**
+     * Log a message to the default log at level DEBUG and and to the trace log
+     * at level INFO if trace is enabled for the mediator.
+     */
+    public void traceOrDebug(Object msg) {
+        if (traceOn) {
+            traceLog.info(msg);
+        }
+        defaultLog.debug(msg);
+    }
+
+    public boolean isTraceTraceEnabled() {
+        return traceOn && traceLog.isTraceEnabled();
+    }
+
+    /**
+     * Log a message to the trace log at level TRACE if trace is enabled for the mediator.
+     */
+    public void traceTrace(Object msg) {
+        if (traceOn) {
+            traceLog.trace(msg);
+        }
+    }
+}

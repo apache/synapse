e5375352145773748413fa220076578175946cf5
Andreas Veithen <veithen@apache.org>
2008-06-26 21:45:19 +0000

    Added the necessary interfaces and concrete classes to implement SYNAPSE-378. I added them to the org.apache.synapse.util.jaxp package because they will probably be useful not only in XSLTMediator, but also e.g. in ValidateMediator.
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk@672034 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/modules/core/src/main/java/org/apache/synapse/util/jaxp/StreamResultBuilder.java
@@ -0,0 +1,94 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *   * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.apache.synapse.util.jaxp;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.Result;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.impl.builder.StAXOMBuilder;
+import org.apache.axiom.om.util.StAXUtils;
+import org.apache.axiom.soap.impl.builder.StAXSOAPModelBuilder;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.synapse.SynapseException;
+import org.apache.synapse.core.SynapseEnvironment;
+import org.apache.synapse.util.TemporaryData;
+
+/**
+ * {@link ResultBuilder} implementation that produces a {@link StreamResult} backed by a
+ * {@link TemporaryData} object.
+ */
+public class StreamResultBuilder implements ResultBuilder {
+    private static final Log log = LogFactory.getLog(StreamResultBuilder.class);
+    
+    private final SynapseEnvironment synEnv;
+    private final boolean isSoapEnvelope;
+    private TemporaryData tmp;
+    private OutputStream out;
+    
+    public StreamResultBuilder(SynapseEnvironment synEnv, boolean isSoapEnvelope) {
+        this.synEnv = synEnv;
+        this.isSoapEnvelope = isSoapEnvelope;
+    }
+
+    public Result getResult() {
+        tmp = synEnv.createTemporaryData();
+        out = tmp.getOutputStream();
+        return new StreamResult(out);
+    }
+
+    public OMElement getNode() {
+        try {
+            out.close();
+        } catch (IOException e) {
+            handleException("Error while closing output stream", e);
+        }
+        XMLStreamReader reader;
+        try {
+            reader = StAXUtils.createXMLStreamReader(tmp.getInputStream());
+        } catch (XMLStreamException e) {
+            handleException("Unable to parse the XML output", e);
+            return null;
+        } catch (IOException e) {
+            handleException("I/O error while reading temporary file", e);
+            return null;
+        }
+        if (isSoapEnvelope) {
+            return new StAXSOAPModelBuilder(reader).getSOAPEnvelope();
+        } else {
+            return new StAXOMBuilder(reader).getDocumentElement();
+        }
+    }
+
+    public void release() {
+        tmp.release();
+    }
+
+    private static void handleException(String message, Throwable ex) {
+        log.error(message, ex);
+        throw new SynapseException(message, ex);
+    }
+}

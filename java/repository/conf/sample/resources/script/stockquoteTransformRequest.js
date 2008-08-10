b413a010355697062972765d232276b077e76c8d
Indika Kumara <indika@apache.org>
2008-08-10 10:25:49 +0000

    fixed an issse with binary and plain text resources update sample 350 to test it out
    git-svn-id: https://svn.eu.apache.org/repos/asf/synapse/trunk@684446 13f79535-47bb-0310-9956-ffa450edef68

--- java/repository/conf/sample/resources/script/stockquoteTransformRequest.js
+++ java/repository/conf/sample/resources/script/stockquoteTransformRequest.js
@@ -27,16 +27,5 @@
         </m:getQuote>);
   }
 
-  function transformResponse(mc) {
-     var symbol = mc.getPayloadXML()..*::symbol.toString();
-     var price = mc.getPayloadXML()..*::last.toString();
-     mc.setPayloadXML(
-        <m:CheckPriceResponse xmlns:m="http://services.samples/xsd">
-	   <m:Code>{symbol}</m:Code>
-	   <m:Price>{price}</m:Price>
-        </m:CheckPriceResponse>);
-  }
-
-
 
 

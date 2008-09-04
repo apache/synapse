1f7fd76b9c092992f986f1b95d9328fcb1976d94
Andreas Veithen <veithen@apache.org>
2008-09-04 22:33:28 +0000

    Transport tests: Introduced TransportTestSuiteBuilder to automatically build a test suite based on a set of environments, clients, channels and endpoints.
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk/java@692284 13f79535-47bb-0310-9956-ffa450edef68

--- modules/transports/src/test/java/org/apache/synapse/transport/mail/MailTransportListenerTest.java
+++ modules/transports/src/test/java/org/apache/synapse/transport/mail/MailTransportListenerTest.java
@@ -19,21 +19,12 @@
 
 package org.apache.synapse.transport.mail;
 
-import static org.apache.synapse.transport.testkit.AdapterUtils.adapt;
-
-import java.util.LinkedList;
-import java.util.List;
-
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import org.apache.synapse.transport.testkit.AdapterUtils;
 import org.apache.synapse.transport.testkit.TransportTestSuite;
-import org.apache.synapse.transport.testkit.client.AsyncTestClient;
+import org.apache.synapse.transport.testkit.TransportTestSuiteBuilder;
 import org.apache.synapse.transport.testkit.client.axis2.AxisAsyncTestClient;
-import org.apache.synapse.transport.testkit.message.MessageDecoder;
-import org.apache.synapse.transport.testkit.message.MessageEncoder;
-import org.apache.synapse.transport.testkit.message.XMLMessage;
 import org.apache.synapse.transport.testkit.server.axis2.AxisAsyncEndpoint;
 import org.apache.synapse.transport.testkit.server.axis2.AxisEchoEndpoint;
 import org.apache.synapse.transport.testkit.server.axis2.AxisServer;
@@ -54,27 +45,31 @@ public class MailTransportListenerTest extends TestCase {
         // SYNAPSE-434
         suite.addExclude("(test=MinConcurrency)");
         
-        MailTestEnvironment env = new GreenMailTestEnvironment();
+        TransportTestSuiteBuilder builder = new TransportTestSuiteBuilder(suite);
         
-        AxisAsyncEndpoint asyncEndpoint = new AxisAsyncEndpoint();
+        GreenMailTestEnvironment env = new GreenMailTestEnvironment();
+        
+        builder.addEnvironment(env);
+        
         MailChannel channel = new MailChannel();
-        suite.addPOXTests(channel, adapt(new MailRequestResponseClient(new FlatLayout()), MessageEncoder.XML_TO_BYTE, MessageDecoder.BYTE_TO_XML), new AxisEchoEndpoint(), env);
-        List<MailAsyncClient> clients = new LinkedList<MailAsyncClient>();
-        clients.add(new MailAsyncClient(new FlatLayout()));
-        clients.add(new MailAsyncClient(new MultipartLayout()));
-        for (MailAsyncClient client : clients) {
-            AsyncTestClient<XMLMessage> xmlClient = adapt(client, MessageEncoder.XML_TO_BYTE);
-            suite.addSOAPTests(channel, xmlClient, adapt(new AxisAsyncEndpoint(), MessageDecoder.AXIS_TO_XML), env);
-            suite.addPOXTests(channel, xmlClient, adapt(new AxisAsyncEndpoint(), MessageDecoder.AXIS_TO_XML), env);
-            suite.addSwATests(channel, xmlClient, adapt(asyncEndpoint, MessageDecoder.AXIS_TO_XML), env);
-            suite.addTextPlainTests(channel, adapt(client, MessageEncoder.STRING_TO_BYTE), AdapterUtils.adapt(asyncEndpoint, MessageDecoder.AXIS_TO_STRING), env);
-            suite.addBinaryTest(channel, client, adapt(asyncEndpoint, MessageDecoder.AXIS_TO_BYTE), env);
-        }
-        AxisAsyncTestClient axisClient = new AxisAsyncTestClient();
-        suite.addSOAPTests(channel, adapt(axisClient, MessageEncoder.XML_TO_AXIS), adapt(new AxisAsyncEndpoint(), MessageDecoder.AXIS_TO_XML), env);
-        suite.addPOXTests(channel, adapt(axisClient, MessageEncoder.XML_TO_AXIS), adapt(new AxisAsyncEndpoint(), MessageDecoder.AXIS_TO_XML), env);
-        suite.addTextPlainTests(channel, adapt(axisClient, MessageEncoder.TEXT_WRAPPER), AdapterUtils.adapt(asyncEndpoint, MessageDecoder.AXIS_TO_STRING), env);
-        suite.addBinaryTest(channel, adapt(axisClient, MessageEncoder.BINARY_WRAPPER), adapt(asyncEndpoint, MessageDecoder.AXIS_TO_BYTE), env);
+        
+        builder.addAsyncChannel(channel);
+        
+        builder.addAxisAsyncTestClient(new AxisAsyncTestClient());
+        builder.addByteArrayAsyncTestClient(new MailAsyncClient(new FlatLayout()));
+        builder.addByteArrayAsyncTestClient(new MailAsyncClient(new MultipartLayout()));
+        
+        builder.addAxisAsyncEndpoint(new AxisAsyncEndpoint());
+        
+        builder.addRequestResponseChannel(channel);
+        
+        builder.addByteArrayRequestResponseTestClient(new MailRequestResponseClient(new FlatLayout()));
+        builder.addByteArrayRequestResponseTestClient(new MailRequestResponseClient(new MultipartLayout()));
+        
+        builder.addEchoEndpoint(new AxisEchoEndpoint());
+        
+        builder.build();
+        
         suite.addTest(new MinConcurrencyTest(AxisServer.INSTANCE, new MailChannel[] { new MailChannel(), new MailChannel() }, 2, true, env));
         return suite;
     }

90c98c1636f5fb82af3b25bcd047e2b6222181d2
Andreas Veithen <veithen@apache.org>
2008-09-11 22:23:26 +0000

    Transport test kit: * Moved all HTTP related code that is independent of the concrete transport implementation (Axis2 or NIO) to a separate package. * Added an HttpTransportTestSuiteBuilder. * Added a test suite for Axis2's SimpleHTTPServer. This allows us to compare the behavior of the two HTTP implementations.
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk/java@694518 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/transports/src/test/java/org/apache/synapse/transport/testkit/http/HttpTransportTestSuiteBuilder.java
@@ -0,0 +1,87 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *   * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.apache.synapse.transport.testkit.http;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.synapse.transport.testkit.TransportDescriptionFactory;
+import org.apache.synapse.transport.testkit.TransportTestSuite;
+import org.apache.synapse.transport.testkit.TransportTestSuiteBuilder;
+import org.apache.synapse.transport.testkit.client.axis2.AxisAsyncTestClient;
+import org.apache.synapse.transport.testkit.client.axis2.AxisRequestResponseTestClient;
+import org.apache.synapse.transport.testkit.client.axis2.AxisTestClientSetup;
+import org.apache.synapse.transport.testkit.listener.AsyncChannel;
+import org.apache.synapse.transport.testkit.server.axis2.AxisAsyncEndpoint;
+import org.apache.synapse.transport.testkit.server.axis2.AxisEchoEndpoint;
+import org.apache.synapse.transport.testkit.server.axis2.AxisServer;
+import org.apache.synapse.transport.testkit.tests.misc.MinConcurrencyTest;
+
+public class HttpTransportTestSuiteBuilder {
+    private final TransportTestSuite suite;
+    private final TransportDescriptionFactory tdf;
+    
+    private final List<AxisTestClientSetup> axisTestClientSetups = new LinkedList<AxisTestClientSetup>();
+    
+    public HttpTransportTestSuiteBuilder(TransportTestSuite suite,
+            TransportDescriptionFactory tdf) {
+        this.suite = suite;
+        this.tdf = tdf;
+    }
+    
+    public void addAxisTestClientSetup(AxisTestClientSetup setup) {
+        axisTestClientSetups.add(setup);
+    }
+    
+    public void build() {
+        TransportTestSuiteBuilder builder = new TransportTestSuiteBuilder(suite);
+        
+        builder.addEnvironment(tdf);
+        
+        HttpChannel channel = new HttpChannel();
+        
+        builder.addAsyncChannel(channel);
+        
+        builder.addByteArrayAsyncTestClient(new JavaNetClient());
+        if (axisTestClientSetups.isEmpty()) {
+            builder.addAxisAsyncTestClient(new AxisAsyncTestClient());
+        } else {
+            for (AxisTestClientSetup setup : axisTestClientSetups) {
+                builder.addAxisAsyncTestClient(new AxisAsyncTestClient(), setup);
+            }
+        }
+        builder.addRESTAsyncTestClient(new JavaNetRESTClient());
+        
+        builder.addAxisAsyncEndpoint(new AxisAsyncEndpoint());
+        builder.addByteArrayAsyncEndpoint(new JettyByteArrayAsyncEndpoint());
+        builder.addRESTAsyncEndpoint(new JettyRESTAsyncEndpoint());
+        
+        builder.addRequestResponseChannel(channel);
+        
+        builder.addAxisRequestResponseTestClient(new AxisRequestResponseTestClient());
+        
+        builder.addEchoEndpoint(new AxisEchoEndpoint());
+        builder.addEchoEndpoint(new JettyEchoEndpoint());
+        
+        builder.build();
+        
+        suite.addTest(new MinConcurrencyTest(AxisServer.INSTANCE, new AsyncChannel[] { new HttpChannel(), new HttpChannel() }, 2, false, tdf));
+    }
+}

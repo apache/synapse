ab35f6f87671eab9ee43815fa10633da75390534
Andreas Veithen <veithen@apache.org>
2008-09-20 20:10:06 +0000

    HTTP transport tests: Monitor the TCP connections and dump data to log files.
    git-svn-id: https://svn.eu.apache.org/repos/asf/synapse/trunk@697416 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/modules/transports/src/test/java/org/apache/synapse/transport/testkit/util/tcpmon/Tunnel.java
@@ -0,0 +1,66 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *   * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.apache.synapse.transport.testkit.util.tcpmon;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Tunnel implements Runnable {
+    private static final Log log = LogFactory.getLog(Tunnel.class);
+    
+    private final ServerSocket serverSocket;
+    private final InetSocketAddress target;
+    
+    public Tunnel(InetSocketAddress target) throws IOException {
+        serverSocket = new ServerSocket(0);
+        this.target = target;
+    }
+    
+    public int getPort() {
+        return serverSocket.getLocalPort();
+    }
+    
+    public void run() {
+        while (true) {
+            Socket socket;
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException ex) {
+                break;
+            }
+            try {
+                Socket targetSocket = new Socket(target.getAddress(), target.getPort());
+                new Thread(new Relay("SENT", socket.getInputStream(), targetSocket.getOutputStream())).start();
+                new Thread(new Relay("RECEIVED", targetSocket.getInputStream(), socket.getOutputStream())).start();
+            } catch (IOException ex) {
+                log.error(ex);
+            }
+        }
+    }
+    
+    public void stop() throws IOException {
+        serverSocket.close();
+    }
+}

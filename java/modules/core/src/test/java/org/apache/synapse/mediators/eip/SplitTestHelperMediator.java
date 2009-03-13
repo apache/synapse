b18ab900dc3b076de438dafa539503235215d5b9
Indika Kumara <indika@apache.org>
2009-03-13 07:16:16 +0000

    remove ManagedLifecycle from mediators that extends AbstractMediator as AbstractMediator already implements ManagedLifecycle
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk@753144 13f79535-47bb-0310-9956-ffa450edef68

--- java/modules/core/src/test/java/org/apache/synapse/mediators/eip/SplitTestHelperMediator.java
+++ java/modules/core/src/test/java/org/apache/synapse/mediators/eip/SplitTestHelperMediator.java
@@ -1,21 +1,20 @@
 package org.apache.synapse.mediators.eip;
 
-import org.apache.synapse.mediators.AbstractMediator;
-import org.apache.synapse.MessageContext;
-import org.apache.synapse.ManagedLifecycle;
-import org.apache.synapse.core.SynapseEnvironment;
-import org.apache.axiom.soap.SOAPEnvelope;
 import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.soap.SOAPEnvelope;
 import org.apache.axis2.AxisFault;
+import org.apache.synapse.MessageContext;
+import org.apache.synapse.core.SynapseEnvironment;
+import org.apache.synapse.mediators.AbstractMediator;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA. User: ruwan Date: Oct 3, 2007 Time: 7:26:09 AM To change this template
  * use File | Settings | File Templates.
  */
-public class SplitTestHelperMediator extends AbstractMediator implements ManagedLifecycle {
+public class SplitTestHelperMediator extends AbstractMediator {
 
     private List mediatedContext = new ArrayList();
     int msgcount;

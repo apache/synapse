0621a9eee636753c057c06cc0c8ea9fcaa317057
Andreas Veithen <veithen@apache.org>
2009-04-12 22:21:45 +0000

    Create the Derby test database used by DBLookupMediatorTest and DBReportMediatorTest in the 'target' directory so that it will be cleaned up by a 'mvn clean'. Maybe this will solve the build failure on builder.wso2.org.
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk/java@764341 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core/src/test/java/org/apache/synapse/mediators/db/DBLookupMediatorTest.java
+++ modules/core/src/test/java/org/apache/synapse/mediators/db/DBLookupMediatorTest.java
@@ -28,7 +28,6 @@ import org.apache.synapse.mediators.AbstractMediatorTestCase;
 import org.apache.synapse.mediators.TestUtils;
 
 import java.sql.SQLException;
-import java.io.File;
 
 public class DBLookupMediatorTest extends AbstractMediatorTestCase {
 
@@ -53,9 +52,10 @@ public class DBLookupMediatorTest extends AbstractMediatorTestCase {
 
             protected void setUp() throws Exception {
 
-                File temp = File.createTempFile("temp", "delete");
-                temp.deleteOnExit();
-                String tempPath = temp.getParent();
+                String baseDir = System.getProperty("basedir");
+                if (baseDir == null) {
+                    baseDir = ".";
+                }
 
                 lookup = (DBLookupMediator)
                     new DBLookupMediatorFactory().createMediator(createOMElement(
@@ -63,7 +63,7 @@ public class DBLookupMediatorTest extends AbstractMediatorTestCase {
                             "  <connection>\n" +
                             "    <pool>\n" +
                             "      <driver>org.apache.derby.jdbc.EmbeddedDriver</driver>\n" +
-                            "      <url>jdbc:derby:" + tempPath + "/derbyDB;create=true</url>\n" +
+                            "      <url>jdbc:derby:" + baseDir + "/target/derbyDB;create=true</url>\n" +
                             "      <user>user</user>\n" +
                             "      <password>pass</password>\n" +
                             "      <property name=\"initialsize\" value=\"2\"/>\n" +

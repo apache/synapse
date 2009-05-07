3a0278f45fdbb2a644bb1ee6b88ac4a5b2e66347
Andreas Veithen <veithen@apache.org>
2009-05-07 22:22:12 +0000

    Fixed another issue in the sample client (see comment in the code).
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk/java@772794 13f79535-47bb-0310-9956-ffa450edef68

--- modules/samples/src/main/java/samples/userguide/StockQuoteCallback.java
+++ modules/samples/src/main/java/samples/userguide/StockQuoteCallback.java
@@ -1,5 +1,6 @@
 package samples.userguide;
 
+import org.apache.axiom.om.OMElement;
 import org.apache.axis2.client.async.AxisCallback;
 
 /**
@@ -9,8 +10,12 @@ public class StockQuoteCallback implements AxisCallback {
 
     public void onMessage(org.apache.axis2.context.MessageContext messageContext) {
         System.out.println("Response received to the callback");
-        StockQuoteClient.InnerStruct.RESULT
+        OMElement result
                 = messageContext.getEnvelope().getBody().getFirstElement();
+        // Detach the result to make sure that the element we return to the sample client
+        // is completely built
+        result.detach();
+        StockQuoteClient.InnerStruct.RESULT = result;
     }
 
     public void onFault(org.apache.axis2.context.MessageContext messageContext) {

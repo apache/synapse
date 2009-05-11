d0e3004d39f19054b2ff7d4ee6b1cf5589b3bab5
Ruwan Linton <ruwan@apache.org>
2009-05-11 18:32:25 +0000

    reverting the default behavior to remove addressing (SYNAPSE-525)
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk/java@773645 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core/src/main/java/org/apache/synapse/SynapseConstants.java
+++ modules/core/src/main/java/org/apache/synapse/SynapseConstants.java
@@ -145,7 +145,7 @@ public final class SynapseConstants {
         public static final String OUT_ONLY = "OUT_ONLY";
         /** The Synapse MC property that states that existing WS-A headers in the envelope should
         * be preserved */
-        public static final String REMOVE_WS_ADDRESSING = "REMOVE_WS_ADDRESSING";
+        public static final String PRESERVE_WS_ADDRESSING = "PRESERVE_WS_ADDRESSING";
 
         //-- error handling --
         /** An Axis2 message context property indicating a transport send failure */

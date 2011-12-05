e9085a4db35bafbda922fe4e94ef0f7447e641c5
Hiranya Kasub Jayathilaka <hiranya@apache.org>
2011-12-05 13:32:28 +0000

    License header updates
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk/java@1210457 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core/src/main/java/org/apache/synapse/message/processors/forward/BlockingMessageSender.java
+++ modules/core/src/main/java/org/apache/synapse/message/processors/forward/BlockingMessageSender.java
@@ -16,6 +16,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
+
 package org.apache.synapse.message.processors.forward;
 
 import org.apache.axiom.om.OMAbstractFactory;

7332b2145712baff4fa8cc4d3a0dff1db9706377
Hiranya Kasub Jayathilaka <hiranya@apache.org>
2013-07-16 21:04:58 +0000

    SYNAPSE-777 Applying the patch from Dave to set a message ID on the message context instances created by SynapseAxisServlet.
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk@1503887 13f79535-47bb-0310-9956-ffa450edef68

--- java/modules/core/src/main/java/org/apache/synapse/core/axis2/SynapseAxisServlet.java
+++ java/modules/core/src/main/java/org/apache/synapse/core/axis2/SynapseAxisServlet.java
@@ -18,7 +18,9 @@
  */
 package org.apache.synapse.core.axis2;
 
+import org.apache.axiom.util.UIDGenerator;
 import org.apache.axis2.context.ConfigurationContext;
+import org.apache.axis2.context.MessageContext;
 import org.apache.axis2.transport.http.AxisServlet;
 import org.apache.axis2.transport.http.ListingAgent;
 import org.apache.synapse.ServerManager;
@@ -27,6 +29,8 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Extends axis2 servlet functionality so that  avoid  starting listeners again
@@ -58,5 +62,22 @@ public class SynapseAxisServlet extends AxisServlet {
 
     public void initContextRoot(HttpServletRequest req) {
         this.configContext.setContextRoot("/");
+    }
+
+    /**
+     * The Axis servlet does not generate a messageId for a new MessageContext. This is needed
+     * at least by the Clone and IterateMediator in order to generate correlationIds that can be
+     * picked up by the AggregateMediator (when no correlation expression is provided).
+     *
+     * @see org.apache.axis2.transport.http.AxisServlet#createMessageContext(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, boolean)
+     */
+    protected MessageContext createMessageContext(HttpServletRequest request,
+                                                  HttpServletResponse response,
+                                                  boolean invocationType) throws IOException {
+    	MessageContext msgCtx = super.createMessageContext(request, response, invocationType);
+        // Set a message ID as Axis2 doesn't seem to set one.
+        // Message ID is required for certain Synapse mediators.
+    	msgCtx.setMessageID(UIDGenerator.generateURNString());
+    	return msgCtx;
     }
 }

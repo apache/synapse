05eab2b3796ea45412b46d486be43a245343cd7f
Hiranya Kasub Jayathilaka <hiranya@apache.org>
2013-07-18 22:23:33 +0000

    Applying the patch for SYNAPSE-887 with some modifications. Adding a test case to verify the fix.
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk@1504692 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/modules/core/src/test/java/org/apache/synapse/mediators/elementary/EnrichMediatorTest.java
@@ -0,0 +1,66 @@
+package org.apache.synapse.mediators.elementary;
+
+import junit.framework.TestCase;
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+import org.apache.synapse.MessageContext;
+import org.apache.synapse.mediators.TestUtils;
+
+import javax.xml.namespace.QName;
+
+public class EnrichMediatorTest extends TestCase {
+
+    public void testEnrich1() throws Exception {
+        OMFactory fac = OMAbstractFactory.getOMFactory();
+        OMElement element = fac.createOMElement(new QName("test"));
+        element.setText("12345");
+
+        EnrichMediator mediator = new EnrichMediator();
+        Source source = new Source();
+        source.setSourceType(EnrichMediator.INLINE);
+        source.setInlineOMNode(element);
+
+        Target target = new Target();
+        target.setTargetType(EnrichMediator.BODY);
+        mediator.setSource(source);
+        mediator.setTarget(target);
+
+        MessageContext msgContext = TestUtils.getTestContext("<empty/>");
+        mediator.mediate(msgContext);
+        OMElement firstElement = msgContext.getEnvelope().getBody().getFirstElement();
+        assertEquals("test", firstElement.getLocalName());
+        assertEquals("12345", firstElement.getText());
+    }
+
+    public void testEnrich2() throws Exception {
+        OMFactory fac = OMAbstractFactory.getOMFactory();
+        OMElement element = fac.createOMElement(new QName("test"));
+        element.setText("12345");
+
+        OMElement propElement = fac.createOMElement(new QName("property"));
+
+        EnrichMediator mediator = new EnrichMediator();
+        Source source = new Source();
+        source.setSourceType(EnrichMediator.INLINE);
+        source.setInlineOMNode(element);
+
+        Target target = new Target();
+        target.setTargetType(EnrichMediator.PROPERTY);
+        target.setProperty("foo");
+        target.setAction(Target.ACTION_ADD_CHILD);
+        mediator.setSource(source);
+        mediator.setTarget(target);
+
+        MessageContext msgContext = TestUtils.getTestContext("<empty/>");
+        msgContext.setProperty("foo", propElement);
+
+        mediator.mediate(msgContext);
+        OMElement firstElement = msgContext.getEnvelope().getBody().getFirstElement();
+        assertEquals("empty", firstElement.getLocalName());
+
+        OMElement result = (OMElement) msgContext.getProperty("foo");
+        assertEquals("property", result.getLocalName());
+        assertEquals("test", result.getFirstElement().getLocalName());
+    }
+}

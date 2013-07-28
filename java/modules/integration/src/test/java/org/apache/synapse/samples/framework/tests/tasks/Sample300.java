477cb6c60aefc3ac17aafcf2d2e88207ada7f82d
Hiranya Kasub Jayathilaka <hiranya@apache.org>
2013-07-28 00:20:25 +0000

    Adding sample300 (scheduled tasks) to the integration test suite.
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk@1507749 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/modules/integration/src/test/java/org/apache/synapse/samples/framework/tests/tasks/Sample300.java
@@ -0,0 +1,18 @@
+package org.apache.synapse.samples.framework.tests.tasks;
+
+import org.apache.synapse.samples.framework.SynapseTestCase;
+
+public class Sample300 extends SynapseTestCase {
+
+    public Sample300() {
+        super(300);
+    }
+
+    public void testRestToPox() throws Exception {
+        log.info("Waiting 10 seconds for the task to run...");
+        Thread.sleep(10000);
+        int messageCount = getAxis2Server().getMessageCount("SimpleStockQuoteService", "getQuote");
+        log.info("Task sent " + messageCount + " messages.");
+        assertTrue(messageCount >= 2);
+    }
+}

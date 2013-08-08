5c4ebbbaec828abdc245eb85a23b3d65433436b2
Hiranya Kasub Jayathilaka <hiranya@apache.org>
2013-08-08 18:02:16 +0000

    Adding a serialization test case for dynamic load balance endpoint
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk/java@1511904 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core/src/main/java/org/apache/synapse/core/axis2/Axis2LoadBalanceMembershipHandler.java
+++ modules/core/src/main/java/org/apache/synapse/core/axis2/Axis2LoadBalanceMembershipHandler.java
@@ -16,6 +16,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
+
 package org.apache.synapse.core.axis2;
 
 import org.apache.axis2.clustering.ClusteringAgent;

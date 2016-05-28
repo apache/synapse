713616d82ef29a1038950756c38f253a1f0e58e6
Andreas Veithen <veithen@apache.org>
2016-05-28 18:43:42 +0000

    Use the proper Axiom API to resolve QNames.
    git-svn-id: https://svn.apache.org/repos/asf/synapse/trunk@1745919 13f79535-47bb-0310-9956-ffa450edef68

--- java/modules/core/src/main/java/org/apache/synapse/config/xml/OMElementUtils.java
+++ java/modules/core/src/main/java/org/apache/synapse/config/xml/OMElementUtils.java
@@ -19,11 +19,9 @@
 
 package org.apache.synapse.config.xml;
 
-import org.apache.axiom.om.OMContainer;
 import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.OMNamespace;
 import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.synapse.SynapseException;
 import org.jaxen.JaxenException;
 import org.jaxen.XPath;
@@ -34,39 +32,6 @@ import java.util.Iterator;
  * Holds Axiom utility methods used by Synapse
  */
 public class OMElementUtils {
-
-    private static final Log log = LogFactory.getLog(OMElementUtils.class);
-
-    /**
-     * Return the namespace with the given prefix, using the given element
-     * @param prefix the prefix looked up
-     * @param elem the source element to use
-     * @return the namespace which maps to the prefix or null
-     */
-    public static String getNameSpaceWithPrefix(String prefix, OMElement elem) {
-        if (prefix == null || elem == null) {
-            log.warn("Searching for null NS prefix and/or using null OMElement");
-            return null;
-        }
-        
-        OMElement currentElem = elem;
-        while (true) {
-            Iterator iter = currentElem.getAllDeclaredNamespaces();
-            while (iter.hasNext()) {
-                OMNamespace ns = (OMNamespace) iter.next();
-                if (prefix.equals(ns.getPrefix())) {
-                    return ns.getNamespaceURI();     
-                }
-            }
-            OMContainer parent = currentElem.getParent();
-            if (parent != null && parent instanceof OMElement) {
-                currentElem = (OMElement)parent;
-            } else {
-                return null;
-            }
-        }
-    }
-
     /**
      * Add the namespace declarations of a given {@link OMElement} to the namespace
      * context of an XPath expression. Typically this method is used with an XPath
